538ab5b74d2c17f5700122c4cef409933520c9fb
Kevin Kim <rkdgus990503@gmail.com>
2024-06-05 21:39:44 -0700

    docker setup and structuring develop environment

---
+++ app/backend/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const { Client } = require('pg');
+require('dotenv').config();
+
+const app = express();
+const port = 3001;
+
+const client = new Client({
+  user: process.env.POSTGRES_USER,
+  host: process.env.DB_HOST,
+  database: process.env.POSTGRES_DB,
+  password: process.env.POSTGRES_PASSWORD,
+  port: process.env.DB_PORT,
+});
+
+client.connect()
+  .then(() => console.log('Connected to PostgreSQL'))
+  .catch(err => console.error('Connection error', err.stack));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Backend server running at http://localhost:${port}`);
+});
