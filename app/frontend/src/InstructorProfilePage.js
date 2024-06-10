50234c582406ad27308bea5ab2f038233fa3ee37
Kevin Kim <rkdgus990503@gmail.com>
2024-06-10 14:32:55 -0700

    fixed some css, finalizing sidebar design (added icons and font size fixed)

--- app/frontend/src/InstructorProfilePage.js
+++ app/frontend/src/InstructorProfilePage.js
@@ -1,24 +1,26 @@
 import React, { useState } from 'react';
 import './InstructorProfilePage.css';
+import { FaHome, FaSmile } from 'react-icons/fa';
 
 function InstructorProfilePage() {
   return (
     <div className="dashboard-container">
       <aside className="sidebar">
         <h2>PEFORMA</h2>
+        <hr className="divider" />
         <ul className="menu">
-            <li><a href="#">Dashboard</a></li>
-            <li><a href="#">Performance</a></li>
+          <li><a href="#"><FaHome className="icon" size={30} /> Dashboard</a></li>
+          <li><a href="#"><FaSmile className="icon" size={30} /> Performance</a></li>
         </ul>
       </aside>
-      <div className='top'>
-        <button className="logout">Logout</button>
+      <div className='content'>
+        <header>
+          <div className="profile">
+          </div>
+          <button className="logout">Logout</button>
+        </header>
       </div>
-
-      
     </div>
-    
-    
   );
 }
 
