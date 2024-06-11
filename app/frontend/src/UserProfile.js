06e5d64f5ffe078846cb6bd3643e9b6b39f5e764
ZhiHengZhang01 <77310385+ZhiHengZhang01@users.noreply.github.com>
2024-06-11 13:45:38 -0700

    Comment out router import
    Comment out the import router line so it can be run and pass the test on github. Comment out should be removed when all pages are connected later, but it can work seperatly to load one page only for now.

--- app/frontend/src/UserProfile.js
+++ app/frontend/src/UserProfile.js
@@ -1,5 +1,5 @@
 import React , {useState} from 'react';
-import { useNavigate } from 'react-router-dom'; 
+//import { useNavigate } from 'react-router-dom'; 
 import './UserProfile.css'; // Importing CSS specific to the UserProfile
 
 
