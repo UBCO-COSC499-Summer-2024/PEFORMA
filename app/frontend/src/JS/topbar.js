54da1a2fc109981a362bd7cd184a63a4ca894ef4
fruitgushers <jeremyadams166@gmail.com>
2024-06-11 12:31:51 -0700

    Figured out how to redirect pages. I commented it out to make it not mess with anything, but it's good for later

--- app/frontend/src/JS/topbar.js
+++ app/frontend/src/JS/topbar.js
@@ -1,7 +1,13 @@
-
+import { useNavigate } from 'react-router-dom';
 function Topbar() { 
+    /*
+    let navigate = useNavigate();
+    const redirect = () => {
+        navigate("/HomePage");
+    }
+    */
         return(
-            <div className="topbar" onClick={() => console.log('Logout')}>
+            <div className="topbar" /*onClick={redirect}*/>
                 <div className="logout">
                     Logout
                 </div>
