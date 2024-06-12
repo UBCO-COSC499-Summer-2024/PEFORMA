1d82a1241c02c09ee64b349466083e35277cd8e2
Kevin Kim <rkdgus990503@gmail.com>
2024-06-11 21:03:58 -0700

    removed commented out lines to use in routing

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -3,14 +3,14 @@
 
 import React from "react";
 import HomePage from "./HomePage";
-//import Login from "./Login";
-//import ForgotPasswordPage from './ForgotPasswordPage';
-//import Dashboard from './Dashboard';
-//import CourseList from './CourseList';
-//import EditProfile from "./EditProfile";
-//import UserProfile from "./UserProfile";
-//import PerformancePage from "./PerformancePage";
-//import CourseHistory from "./CourseHistory";
+import Login from "./Login";
+import ForgotPasswordPage from './ForgotPasswordPage';
+import Dashboard from './Dashboard';
+import CourseList from './CourseList';
+import EditProfile from "./EditProfile";
+import UserProfile from "./UserProfile";
+import PerformancePage from "./PerformancePage";
+import CourseHistory from "./CourseHistory";
 import { BrowserRouter ,Router, Route, Routes} from 'react-router-dom';
 
 function App() {
