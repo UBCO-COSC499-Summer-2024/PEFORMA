e77a4000566d0cf45b32f89eb570ef4bc24ee3d0
fruitgushers <jeremyadams166@gmail.com>
2024-06-14 13:29:15 -0700

    Got each division to link to course list

--- app/frontend/src/JS/Instructor/Dashboard.js
+++ app/frontend/src/JS/Instructor/Dashboard.js
@@ -1,38 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '../../CSS/Instructor/Dashboard.css';
 import CreateSidebar, { CreateTopbar } from '../commonImports.js';
+import divisions from '../common/divisions.js';
+import { useNavigate, Link } from 'react-router-dom';
 
 function Dashboard() {
+  let navigate = useNavigate();
+  
+  
+  
   return (
     <div className="dashboard">
       <CreateSidebar />
       <div className="container">
       <CreateTopbar />
-        <header className='web-head'>
-          <input type="search" placeholder="Search by Subject (e.g. CS123) or Instructor name (e.g. Chipinski)" />
-          
-        </header>
+        
         <div className="card-container">
-          <div className="card">
-            {/* image path replace needed , total 4,saved in public folder*/} 
-            <img src="./temp.png" alt="Computer Science" />
-            <div>Computer Science</div>
-          </div>
-
-          <div className="card">
-            <img src="./temp.png" alt="Mathematics" />
-            <div>Mathematics</div>
-          </div>
-
-          <div className="card">
-            <img src="./temp.png" alt="Physics" />
-            <div>Physics</div>
-          </div>
-
-          <div className="card">
-            <img src="./temp.png" alt="Statistics" />
-            <div>Statistics</div>
-          </div>
+          {divisions.map(division => {
+            return (<Link to={"/CourseList?division="+division.code} key={division.code}><div className="card">
+                  
+                      <img src="./temp.png" alt={division.label} />
+                      <div>{division.label}</div>
+                    </div></Link>
+                    );
+          })}
         </div>
       </div>
     </div>
