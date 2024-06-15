fc86f3b92e81cfe8dea039d188715508353ae4e9
Kevin Kim <rkdgus990503@gmail.com>
2024-06-14 18:23:50 -0700

    setting up basic layouts

--- app/frontend/src/JS/Instructor/PerformanceInstructorPage.js
+++ app/frontend/src/JS/Instructor/PerformanceInstructorPage.js
@@ -1,42 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import '../../CSS/Instructor/PerformanceInstructorPage.css';
 import CreateSidebar, { CreateTopbar } from '../commonImports.js';
 
 
 function UserProfile() {
+  const params = new URLSearchParams(window.location.search);
+  const ubcid = params.get('ubcid');
+
+  const initProfile = {"roles":[],"teachingAssignments":[{}]};
+  const [profile, setProfile] = useState(initProfile)
+
+  useEffect(() => {
+    const fetchData = async() => {
+      const res = await axios.get('http://localhost:3000/profileSample.json?ubcid=' + ubcid); //replace it to api
+      return res.data;    
+    }
+    fetchData().then(res => setProfile(res));
+  }, []);
+
   return (
     <div className="dashboard-container">
       <CreateSidebar />
 
       <div className="container">
           <CreateTopbar />
+          <div className='info-table'>
+            <section className='information'>
+              <h1>Welcome {profile.name}!</h1>
+              <p><strong>Name:</strong> {profile.name}</p>
+              <p><strong>UBC ID:</strong> {profile.ubcid}</p>
+              <p><strong>Service Roles:</strong> {profile.roles.map(role => role).join(', ')}</p>
+              <p><strong>Monthly Hours Benchmark:</strong> {profile.benchmark}</p>
+              <p><strong>Email:</strong> {profile.email}</p>
+              <p><strong>Teaching Assignments:</strong> 
+                {profile.teachingAssignments.map(teachingAssign => (
+                <a href='{teachingAssign.link}'> {teachingAssign.assign}</a>
+                )).reduce((prev, curr) => [prev, ', ', curr])}
+              </p>
+            </section>
+
+            <div className="graph-section">
+            
+            </div>
+          </div>
 
         <div className="profile-details">
           <div className="profile-info">
-          <h1>Billy Guy's Profile</h1>
-            <p>Name: Billy Guy</p>
-            <p>UBC ID: 123413234</p>
-            <p>Service Role: Professor</p>
-            <p>Monthly Hours Benchmark: 1192</p>
-            <p>Email: billyguy123@ubc.ca</p>
-            <p>Phone Number: 250-123-1234</p>
-            <p>Office Location: Alaska</p>
-            <p>Teaching Courses:</p>
-            
-            <p>MATH 064<br/>MATH 745</p>
-            {/* change course by data↑ */}
-
             <div className="score">
               <h2>Billy Guy's Score:</h2>
               <h2 className='compare'>85.7 </h2>
               <h2 className='compare'> ↑ </h2>
             </div>
           </div>
-          <div className="graph-section">
-            <img src='temp.png' className='graph'/>
-            <button>Graph Options</button>
-          </div>
         </div>
+        
+        <div className='dept-info'>
+          <h1>Department Performance</h1>
+
+        </div>
+          
       </div>
     </div>
   );
