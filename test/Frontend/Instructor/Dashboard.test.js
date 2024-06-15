d3ff853bfc69a8ac69756e31474c82882f647130
fruitgushers <jeremyadams166@gmail.com>
2024-06-15 13:16:19 -0700

    Fixed typeError

--- test/Frontend/Instructor/Dashboard.test.js
+++ test/Frontend/Instructor/Dashboard.test.js
@@ -2,15 +2,16 @@ import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import divisions from '../../../app/frontend/src/JS/common/divisions';
 import Dashboard from '../../../app/frontend/src/JS/Instructor/Dashboard';
+import {MemoryRouter} from "react-router-dom";
 
 test('renders dashboard card image', () => {
-  render(<Dashboard />);
+  render(<MemoryRouter><Dashboard /></MemoryRouter>);
   const cardImg = screen.getByAltText("Computer Science");
   expect(cardImg).toBeInTheDocument();
 });
 
 test('Ensure all divisions are listed', () => {
-    render(<Dashboard />);
+    render(<MemoryRouter><Dashboard /></MemoryRouter>);
     const cards = screen.getAllByRole("gridcell");
     expect(cards.length).toBe(divisions.length);
 });
