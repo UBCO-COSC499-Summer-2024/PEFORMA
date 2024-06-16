30e4d339763d6cf52609a4f7780518f49636d0e6
Adams Chen <ayhchen96@gmail.com>
2024-06-16 12:16:50 -0700

    Changed filename "ddl" to "sql" and updated dockerfile to reflect that change

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,7 +1,7 @@
 -- Dropping existing tables to clear
 DROP TABLE IF EXISTS "SurveyType" CASCADE;
 DROP TABLE IF EXISTS "Division" CASCADE;
-DROP TABLE IF EXISTS "Image" CASCADE;
+-- DROP TABLE IF EXISTS "Image" CASCADE;
 DROP TABLE IF EXISTS "Profile" CASCADE;
 DROP TABLE IF EXISTS "Account" CASCADE;
 DROP TABLE IF EXISTS "ServiceRole" CASCADE;
@@ -20,13 +20,6 @@ DROP TABLE IF EXISTS "SurveyQuestionResponse" CASCADE;
 CREATE TABLE "Division" (
   "divisionId" SERIAL PRIMARY KEY,
   "dname"       varchar(50)
-);
-
--- Create images
-CREATE TABLE "Image" (
-  "imageId"     SERIAL PRIMARY KEY,
-  "file_type"   varchar(3),
-  "image_data"  BYTEA
 );
 
 -- Create profiles
@@ -50,6 +43,13 @@ CREATE TABLE "Profile" (
   UNIQUE ("profileId", "email")
 );
 
+-- -- Create images
+-- CREATE TABLE "Image" (
+--   "imageId"     SERIAL PRIMARY KEY,
+--   "file_type"   char(3),
+--   "image_data"  BYTEA
+-- );
+
 -- Create accounts
 CREATE TABLE "Account" (
   "accountId"   SERIAL PRIMARY KEY,
@@ -58,6 +58,14 @@ CREATE TABLE "Account" (
   "password"    varchar(15),
   "isActive"    boolean,
   FOREIGN KEY ("profileId", "email") REFERENCES "Profile" ("profileId", "email")
+);
+
+-- Create account types
+CREATE TABLE "AccountType" (
+  "accountId"     integer,
+  "accountType"   integer,
+  PRIMARY KEY ("accountId", "accountType"),
+  FOREIGN KEY ("accountId") REFERENCES "Account" ("accountId")
 );
 
 -- Create service roles
@@ -70,22 +78,62 @@ CREATE TABLE "ServiceRole" (
   FOREIGN KEY ("divisionId") REFERENCES "Division" ("divisionId")
 );
 
+-- Create service role by year
+CREATE TABLE "ServiceRoleByYear" (
+  "serviceRoleId"   integer,
+  "year"            integer,
+  "JANHour"         double precision,
+  "FEBHour"         double precision,
+  "MARHour"         double precision,
+  "APRHour"         double precision,
+  "MAYHour"         double precision,
+  "JUNHour"         double precision,
+  "JULHour"         double precision,
+  "AUGHour"         double precision,
+  "SEPHour"         double precision,
+  "OCTHour"         double precision,
+  "NOVHour"         double precision,
+  "DECHour"         double precision,
+  PRIMARY KEY ("serviceRoleId", "year"),
+  FOREIGN KEY ("serviceRoleId") REFERENCES "ServiceRole" ("serviceRoleId")
+);
+
+-- Create service role assignments
+CREATE TABLE "ServiceRoleAssignment" (
+  "profileId"       integer,
+  "serviceRoleId"   integer,
+  "year"            integer,
+  PRIMARY KEY ("profileId", "serviceRoleId", "year"),
+  FOREIGN KEY ("profileId") REFERENCES "Profile" ("profileId"),
+  FOREIGN KEY ("serviceRoleId") REFERENCES "ServiceRole" ("serviceRoleId")
+);
+
 -- Create courses
 CREATE TABLE "Course" (
   "courseId"      SERIAL PRIMARY KEY,
   "ctitle"        varchar(50),
-  "description"   varchar(1000),
+  "description"   TEXT,
   "divisionId"    integer,
   "courseNum"     integer,
   FOREIGN KEY ("divisionId") REFERENCES "Division" ("divisionId")
 );
 
--- Create course terms
+-- Create courses by terms
 CREATE TABLE "CourseByTerm" (
   "courseId"      integer,
-  "term"          varchar(9),
+  "term"          integer,    -- Ex. 2024W1 = 20241, 2024W2 = 20242, 2024S1 = 20243, 2024S2 = 20244
   PRIMARY KEY ("courseId", "term"),
   FOREIGN KEY ("courseId") REFERENCES "Course" ("courseId")
+);
+
+-- Create instructor teaching assignments
+CREATE TABLE "InstructorTeachingAssignment" (
+  "profileId"   integer,
+  "courseId"    integer,
+  "term"        integer,
+  PRIMARY KEY ("profileId", "courseId", "term"),
+  FOREIGN KEY ("profileId") REFERENCES "Profile" ("profileId"),
+  FOREIGN KEY ("courseId", "term") REFERENCES "CourseByTerm" ("courseId", "term")
 );
 
 -- Create survey types
@@ -108,7 +156,7 @@ CREATE TABLE "SurveyQuestionResponse" (
   "surveyTypeId"      integer,
   "surveyQuestionId"  integer,
   "courseId"          integer,
-  "term"              varchar(9),
+  "term"              integer,
   "profileId"         integer,
   "studentId"         integer,
   "response"          varchar(500),
@@ -117,62 +165,14 @@ CREATE TABLE "SurveyQuestionResponse" (
   FOREIGN KEY ("profileId") REFERENCES "Profile" ("profileId")
 );
 
-
--- Create instructor teaching assignments
-CREATE TABLE "InstructorTeachingAssignment" (
-  "profileId"   integer,
-  "courseId"    integer,
-  "term"        varchar(9),
-  PRIMARY KEY ("profileId", "courseId", "term"),
-  FOREIGN KEY ("profileId") REFERENCES "Profile" ("profileId"),
-  FOREIGN KEY ("courseId", "term") REFERENCES "CourseByTerm" ("courseId", "term")
-);
-
 -- Create single teaching performances
 CREATE TABLE "SingleTeachingPerformance" (
   "profileId"   integer,
   "courseId"    integer,
-  "term"        varchar(9),
+  "term"        integer,
   "score"       double precision,
   PRIMARY KEY ("profileId", "courseId", "term"),
   FOREIGN KEY ("profileId") REFERENCES "Profile" ("profileId"),
   FOREIGN KEY ("courseId", "term") REFERENCES "CourseByTerm" ("courseId", "term")
 );
 
--- Create service role assignments
-CREATE TABLE "ServiceRoleAssignment" (
-  "profileId"       integer,
-  "serviceRoleId"   integer,
-  "year"            integer,
-  PRIMARY KEY ("profileId", "serviceRoleId", "year"),
-  FOREIGN KEY ("profileId") REFERENCES "Profile" ("profileId"),
-  FOREIGN KEY ("serviceRoleId") REFERENCES "ServiceRole" ("serviceRoleId")
-);
-
--- Create account types
-CREATE TABLE "AccountType" (
-  "accountId"     integer,
-  "accountType"   integer,
-  PRIMARY KEY ("accountId", "accountType"),
-  FOREIGN KEY ("accountId") REFERENCES "Account" ("accountId")
-);
-
--- Create service role by year
-CREATE TABLE "ServiceRoleByYear" (
-  "serviceRoleId"   integer,
-  "year"            integer,
-  "JANHour"         double precision,
-  "FEBHour"         double precision,
-  "MARHour"         double precision,
-  "APRHour"         double precision,
-  "MAYHour"         double precision,
-  "JUNHour"         double precision,
-  "JULHour"         double precision,
-  "AUGHour"         double precision,
-  "SEPHour"         double precision,
-  "OCTHour"         double precision,
-  "NOVHour"         double precision,
-  "DECHour"         double precision,
-  PRIMARY KEY ("serviceRoleId", "year"),
-  FOREIGN KEY ("serviceRoleId") REFERENCES "ServiceRole" ("serviceRoleId")
-);
