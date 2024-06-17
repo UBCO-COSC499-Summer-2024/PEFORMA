b8eeb902bb3ec90f8f7beae25a8b625066c59035
Kevin Kim <rkdgus990503@gmail.com>
2024-06-16 19:14:05 -0700

    styling performance page

--- README.md
+++ README.md
@@ -21,3 +21,11 @@ Please use the provided folder structure for your docs (project plan, design doc
 You can find additional information on folder structure convetions [here](https://github.com/kriasoft/Folder-Structure-Conventions). 
 
 Also, update your README.md file with the team and client/project information.  You can find details on writing GitHub Markdown [here](https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax) as well as a [handy cheatsheet](https://enterprise.github.com/downloads/en/markdown-cheatsheet.pdf).   
+
+
+vscode setting
+
+```bash
+Default Formatter: Prettier - Code Formatter (esbenp.prettier-vscode)
+editor.formatOnSave: true
+```
--- app/frontend/src/JS/Instructor/PerformanceInstructorPage.js
+++ app/frontend/src/JS/Instructor/PerformanceInstructorPage.js
@@ -1,77 +1,96 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import '../../CSS/Instructor/PerformanceInstructorPage.css';
-import CreateSidebar, { CreateLeaderboardChart, CreateScorePolarChart, CreateTopbar, CreateWorkingBarChart } from '../commonImports.js';
-
+import CreateSidebar, {
+	CreateLeaderboardChart,
+	CreateScorePolarChart,
+	CreateTopbar,
+	CreateWorkingBarChart,
+} from '../commonImports.js';
 
 function UserProfile() {
-  const params = new URLSearchParams(window.location.search);
-  const ubcid = params.get('ubcid');
+	const params = new URLSearchParams(window.location.search);
+	const ubcid = params.get('ubcid');
 
-  const initProfile = {"roles":[],"teachingAssignments":[{}]};
-  const [profile, setProfile] = useState(initProfile)
+	const initProfile = {
+		roles: [],
+		teachingAssignments: [{}],
+	};
+	const [profile, setProfile] = useState(initProfile);
 
-  useEffect(() => {
-    const fetchData = async() => {
-      const res = await axios.get('http://localhost:3000/profileSample.json?ubcid=' + ubcid); //replace it to api
-      return res.data;    
-    }
-    fetchData().then(res => setProfile(res));
-  }, []);
+	useEffect(() => {
+		const fetchData = async () => {
+			const res = await axios.get('http://localhost:3000/profileSample.json?ubcid=' + ubcid); //replace it to api
+			return res.data;
+		};
+		fetchData().then((res) => setProfile(res));
+	}, []);
 
-  return (
-    
-    <div className="dashboard-container">
-      <CreateSidebar />
+	return (
+		<div className="dashboard-container">
+			<CreateSidebar />
 
-      <div className="container">
-          <CreateTopbar />
-          <div className='greeting'>
-            <h1>Welcome {profile.name}!</h1>
-            <h2>Check out your performance!</h2>
-          </div>
-          
+			<div className="container">
+				<CreateTopbar />
+				<div className="greeting">
+					<h1>Welcome {profile.name}, check out your performance!</h1>
+				</div>
 
-          <div className='info-table'>
-            <section className='info-section'>
-              <h2 className='subTitle'>Your Information</h2>
-              <p><strong>Name:</strong> {profile.name}</p>
-              <p><strong>UBC ID:</strong> {profile.ubcid}</p>
-              <p><strong>Service Roles:</strong> {profile.roles.map(role => role).join(', ')}</p>
-              <p><strong>Monthly Hours Benchmark:</strong> {profile.benchmark}</p>
-              <p><strong>Email:</strong> {profile.email}</p>
-              <p className='teaching-margin'><strong>Teaching Assignments:</strong></p>
-              <ul className='teaching-assignments'>
-                {profile.teachingAssignments.map(teachingAssign => (
-                  <li key={teachingAssign.id}>
-                    <a href='{teachingAssign.link}'> {teachingAssign.assign}</a>
-                  </li>
-                ))}
-              </ul>
-            </section>
+				<div className="info-table">
+					<section className="info-section">
+						<div className="info">
+							<h2 className="subTitle">Your Information</h2>
+							<p>
+								<strong>Name:</strong> {profile.name}
+							</p>
+							<p>
+								<strong>UBC ID:</strong> {profile.ubcid}
+							</p>
+							<p>
+								<strong>Service Roles:</strong> {profile.roles.map((role) => role).join(', ')}
+							</p>
+							<p>
+								<strong>Monthly Hours Benchmark:</strong> {profile.benchmark}
+							</p>
+							<p>
+								<strong>Email:</strong> {profile.email}
+							</p>
+						</div>
 
-            <div className="graph-section">
-              <h2 className='subTitle'>Working Hours</h2>
-              <CreateWorkingBarChart />
-            </div>
-          </div>
+						<div className="assignment-table">
+							<ul className="teaching-assignments">
+								<p className="teaching-margin">
+									<h2 className='subTitle'>Teaching Assignments:</h2>
+								</p>
+								{profile.teachingAssignments.map((teachingAssign) => (
+									<li key={teachingAssign.id}>
+										<a href="{teachingAssign.link}"> {teachingAssign.assign}</a>
+									</li>
+								))}
+							</ul>
+						</div>
+					</section>
 
-        <div className='bottom-section'>
+					<div className="graph-section">
+						<h2 className="subTitle">Working Hours</h2>
+						<CreateWorkingBarChart />
+					</div>
+				</div>
 
-          <div className='polarchart-section'>
-            <h2 className='subTitle'>Department Performance</h2>
-            <CreateScorePolarChart />
-          </div>
+				<div className="bottom-section">
+					<div className="polarchart-section">
+						<h2 className="subTitle">Department Performance</h2>
+						<CreateScorePolarChart />
+					</div>
 
-          <div className='leaderboard-section'>
-            <h2 className='subTitle'>Leader Board (Updated per month)</h2>
-            <CreateLeaderboardChart />
-          </div>
-        </div>
-          
-      </div>
-    </div>
-  );
+					<div className="leaderboard-section">
+						<h2 className="subTitle">Leader Board (Updated per month)</h2>
+						<CreateLeaderboardChart />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default UserProfile;
