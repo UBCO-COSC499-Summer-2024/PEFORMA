0fb3841bbd4496d17b664e10c2afb1133a68754b
subarusakashita <77289992+subarusakashita@users.noreply.github.com>
2024-06-18 05:22:58 +0900

    Adjusted the format to the most recent development branch

--- app/backend/app.js
+++ app/backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const loginRouter = require('./routes/logincheck'); // 确保路径正确
+const profileRoutes = require('./routes/profileRoutes');
 //const serverRouter = require('./routes/server')
 const authenticateRouter = require('./Manager/authenticate');
 const queryAccountRouter = require('./routes/queryAccountRouter').router;
@@ -30,6 +31,10 @@ app.use('/',AccountTypeRouter);//check account type
 
 //update date into db
 //app.use('/api',update);
+
+
+//Profile BE
+app.use('/api/instructorProfile', profileRoutes);
 
 console.log('after');
 
