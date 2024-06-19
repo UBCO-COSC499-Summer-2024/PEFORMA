2026570817c23f8dc6f3a2394e978655fee90022
Adams Chen <ayhchen96@gmail.com>
2024-06-18 18:11:36 -0700

    Implemented "ALL" option support for filtering course list, but need to troubleshoot

--- app/backend/services/courseService.js
+++ app/backend/services/courseService.js
@@ -1,6 +1,7 @@
 const pool = require('../config/db.js');
 
 const divisionMap = {
+    'ALL': 0,
     'COSC': 1, 
     'MATH': 2,
     'PHYS': 3,
@@ -8,6 +9,7 @@ const divisionMap = {
 };
 
 const divisionLabelMap = {
+    0: 'All',
     1: 'Computer Science',
     2: 'Mathematics',
     3: 'Physics',
@@ -25,33 +27,40 @@ async function getFormattedCourseData(divisionCode) {
         SELECT 
             (SELECT COUNT(*) 
             FROM public."Course" c2
-            WHERE c2."divisionId" = $1) AS division_courses_count,
-            c."courseNum" AS course_number, c."ctitle" AS course_title,
+            WHERE ($1 = 0 OR c2."divisionId" = $1)) AS division_courses_count,
+            c."courseNum" AS course_number, 
+            c."ctitle" AS course_title,
             ARRAY_AGG(p."firstName" || ' ' || p."lastName") AS instructor,
-            ARRAY_AGG(p."UBCId") AS ubcid,
-            ARRAY_AGG(p."email") AS email
+            ARRAY_AGG(p."UBCId") AS ubcid, 
+            ARRAY_AGG(p."email") AS email,
+            c."divisonId" AS division_id
         FROM public."Course" c
         JOIN public."InstructorTeachingAssignment" a ON c."courseId" = a."courseId"
         JOIN public."Profile" p ON p."profileId" = a."profileId"
-        WHERE c."divisionId" = $1 AND a."term" = $2
+        WHERE ($1 = 0 OR c."divisionId" = $1) AND a."term" = $2  
         GROUP BY c."courseNum", c."ctitle"
         ORDER BY c."courseNum" ASC;
     `, [divisionId, currTerm]);
 
     // Reformat the data
     const formattedData = {
-        division: divisionCode,
-        divisionLabel: divisionLabel,
+        division: divisionCode, 
+        divisionLabel: divisionLabel, 
         currPage: 1,
         perPage: 10,
         divisionCoursesCount: result.rows.length > 0 ? result.rows[0].division_courses_count : 0,
-        courses: result.rows.map(row => ({  
-            id: `${divisionCode} ${row.course_number}`,
-            title: row.course_title,
-            instructor: row.instructor,
-            ubcid: row.ubcid,
-            email: row.email
-        })) 
+        courses: result.rows.map(row => {
+            const courseDivisionId = row.division_id;
+            const courseDivisionCode = Object.keys(divisionMap).find(key => divisionMap[key] === courseDivisionId); // Find the division code from the ID
+
+            return {  
+                id: `${courseDivisionCode} ${row.course_number}`, // Use courseDivisionCode
+                title: row.course_title,
+                instructor: row.instructor,
+                ubcid: row.ubcid,
+                email: row.email
+            };
+        }) 
     };
 
     // console.log(formattedData);
