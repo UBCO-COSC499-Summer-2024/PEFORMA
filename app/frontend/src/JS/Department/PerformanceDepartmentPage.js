dfb466de2f7d91978d261c162cc5aecdacb09065
Kevin Kim <rkdgus990503@gmail.com>
2024-06-25 18:33:39 -0700

    overview of each divisions rank

--- app/frontend/src/JS/Department/PerformanceDepartmentPage.js
+++ app/frontend/src/JS/Department/PerformanceDepartmentPage.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import '../../CSS/Department/PerformanceDepartmentPage.css';
-import { CreateSidebarDept,
-	CreateTopbar
-} from '../commonImports.js';
+import { CreateSidebarDept,CreateTopbar } from '../commonImports.js';
+import  CoscTable  from './PerformanceImports/CoscTable.js';
+import  MathTable  from './PerformanceImports/MathTable.js';
+import  PhysTable  from './PerformanceImports/PhysTable.js';
+import  StatTable  from './PerformanceImports/StatTable.js';
 
 function PerformanceDepartmentPage() {
 	const params = new URLSearchParams(window.location.search);
@@ -29,54 +31,39 @@ function PerformanceDepartmentPage() {
 
 			<div className="container">
 				<CreateTopbar />
-				<div className="greeting">
-
+				<div className="performanceD-title">
+          <h2>Department Performance Overview</h2>
 				</div>
 
-				<div className="info-table">
-					<section className="info-section">
-						<div className="info">
-							<h2 className="subTitle">Your Information</h2>
-							<p>
-								<strong>Name:</strong> {profile.name}
-							</p>
-							<p>
-								<strong>UBC ID:</strong> {profile.ubcid}
-							</p>
-							<p>
-								<strong>Service Roles:</strong> {profile.roles.map((role) => role).join(', ')}
-							</p>
-							<p>
-								<strong>Monthly Hours Benchmark:</strong> {profile.benchmark}
-							</p>
-							<p>
-								<strong>Email:</strong> {profile.email}
-							</p>
-						</div>
+				<div className="division-top-table">
 
-						<div className="assignment-table">
-							<ul className="teaching-assignments">
-								<p className="teaching-margin">
-									<h2 className='subTitle'>Teaching Assignments:</h2>
-								</p>
-								{profile.teachingAssignments.map((teachingAssign) => (
-									<li key={teachingAssign.id}>
-										<a href="{teachingAssign.link}"> {teachingAssign.assign}</a>
-									</li>
-								))}
-							</ul>
-						</div>
-					</section>
-
-					<div className="graph-section">
+          <div className="division">
+            <CoscTable />
 					</div>
+
+					<div className="division">
+            <MathTable />
+					</div>
+          
 				</div>
+
+        <div className="division-mid-table">
+
+          <div className="division">
+            <PhysTable />
+					</div>
+
+					<div className="division">
+            <StatTable />
+					</div>
+
+        </div>
 
 				<div className="bottom-section">
-					<div className="polarchart-section">
+					<div className="benchmark-section">
 					</div>
 
-					<div className="leaderboard-section">
+					<div className="people-section">
 					</div>
 					
 				</div>
---
+++ app/frontend/src/JS/Department/PerformanceImports/MathTable.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react';
+import '../../../CSS/Department/PerformanceImports/PerformanceDeptTables.css';
+import axios from 'axios';
+
+function MathTable() {
+
+  const [course,setCourse] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async() => {
+      try {
+        const url = "http://localhost:3000/mathDeptP.json";
+        const res = await axios.get(url);
+        setCourse(res.data.courses)
+      } catch (error) {
+        console.log("Error fetching data: ", error)
+      }
+    };
+    fetchData();
+  }, []);
+
+
+  return (
+
+    <div className="division-performance-table">
+      <h1 className='subTitleD'>Mathmatics</h1>
+      <table className='divi-table'>
+        <thead>
+          <tr>
+            <th>NO.</th>
+            <th>Course</th>
+            <th>Rank</th>
+            <th>Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          {course.map((course, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{course.courseCode}</td>
+              <td>{course.rank}</td>
+              <td>{course.score}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+
+  );
+}
+
+export default MathTable;
