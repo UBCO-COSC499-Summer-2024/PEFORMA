e63948a299676be4b8ca5874675afd56cf36ae2a
ZhiHengZhang01 <77310385+ZhiHengZhang01@users.noreply.github.com>
2024-06-26 16:06:44 -0700

    Delete app/backend/controllers/profileRoutes.js

--- app/backend/controllers/profileRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getUserProfile } = require('../controllers/profileController'); // Adjust the path as necessary
-router.get('/', getUserProfile);
-
-module.exports = router;
