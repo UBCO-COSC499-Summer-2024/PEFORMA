2f7cf18d9c474ce986ac8539d4b71efc59d4f9b4
Adams Chen <ayhchen96@gmail.com>
2024-06-27 21:36:57 -0700

    fixed course list

--- app/backend/app.js
+++ app/backend/app.js
@@ -14,7 +14,7 @@ const { upsertProfile } = require('./routes/upsertProfile');
 const { createAccount } = require('./routes/createAccount');
 const { assignServiceRole } = require ('./routes/assignServiceRole');
 
-const workingHoursRoutes = require('./routes/workingHoursRoutes');
+//const workingHoursRoutes = require('./routes/workingHoursRoutes');
 //const serverRouter = require('./routes/server')
 const DeptPerformanceRouter = require('./routes/deptPerformanceRoutes');
 const leaderBoardRoutes = require('./routes/leaderBoardRoutes');
@@ -44,17 +44,8 @@ app.use('/',AccountTypeRouter);//check account type
 //Profile BE
 app.use('/api/instructorProfile',profileRoutes);
 
-//instructor performance BE
-app.use('/api/workingHoursRoutes',workingHoursRoutes);
-app.use('/api/deptPerformance',DeptPerformanceRouter);
-app.use('/api/leaderBoardRoutes',leaderBoardRoutes);
-app.use('/api/progressRoutes',progressRoutes);
-
 // Service role retrieval process
-app.use('/api/service-roles', serviceRoleRoutes);
-
-//Courses
-app.use/('/api/courses',courseRoutes);
+app.use('/api/courses', courseRoutes);
 
 //reset password
 //app.use('/api',ResetPassword);
@@ -76,7 +67,21 @@ app.post('/create-account', async (req, res) => {
     //res.send('Data received successfully');  // 响应前端
 });
 /*
+app.post('/create-account', async (req, res) => {
+    console.log('Received data:', req.body);  // 打印接收到的数据
+    //res.send('Data received successfully');  // 响应前端
 
+//Profile BE
+app.use('/api/workingHoursRoutes',workingHoursRoutes);
+app.use('/api/deptPerformance',DeptPerformanceRouter);
+app.use('/api/lead7erBoardRoutes',leaderBoardRoutes);
+app.use('/api/progressRoutes',progressRoutes);
+
+
+//Performance BE
+//app.use('/api/instructorPerformance',performanceRoutes);
+// Service role retrieval process
+app.use('/api/service-roles', serviceRoleRoutes);
 
 
     try {
