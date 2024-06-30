0511c7dc5b32e95b56ac9157a1bcc316407fac7f
Kevin Kim <rkdgus990503@gmail.com>
2024-06-30 13:23:16 -0700

    tested with mocked data to be rendered properly

--- test/Frontend/Instructor/CourseList.test.js
+++ test/Frontend/Instructor/CourseList.test.js
@@ -1,53 +1,49 @@
-import {act, fireEvent, render, screen, waitFor } from '@testing-library/react';
+import {render, screen, waitFor, element } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import CourseList from '../../../app/frontend/src/JS/Instructor/CourseList';
 import {MemoryRouter} from "react-router-dom";
 import axios from 'axios';
-import { AuthProvider } from '../../../app/frontend/src/JS/AuthContext';
+import { useAuth } from '../../../app/frontend/src/JS/AuthContext';
 
 jest.mock('axios');
+jest.mock('../../../app/frontend/src/JS/AuthContext');
 
-axios.get.mockResolvedValue({
-  data: {"division":"MATH", "divisionLabel":"Mathmatics", "currentPage":1, "perPage": 10, "divisionCoursesCount":1,
-    "courses":[
-      { "id": "MATH 100", "title": "Differential Calculus with Applications to Physical Sciences and Engineering", "instructor": ["Brandi Floyd"], "ubcid":[32819340], "email": ["brandi@instructor.ubc.ca"] }
-    ]
-}
-});
+describe('CourseList', () => {
+	beforeEach(() => {
+		useAuth.mockReturnValue({
+			authToken: { token: 'mocked-token' },
+		});
+	});
+	test('Testing course list to be rendered with mock data', async () => {
+		
+		axios.get.mockImplementation(() => 
+			Promise.resolve({
+				data: {"division":"MATH", "divisionLabel":"Mathmatics", "currentPage":1, "perPage": 10, "divisionCoursesCount":1,
+          "courses":[
+            { "id": "MATH 100", "title": "Differential Calculus with Applications to Physical Sciences and Engineering", "instructor": ["Brandi Floyd"], "ubcid":[32819340], "email": ["brandi@instructor.ubc.ca"] }
+          ]
+        },
+			})
+		);
 
-const renderWithProviders = (ui, { route = '/?division=COSC' } = {}) => {
-  window.history.pushState({}, 'Test page', route);
-  
-  return render(
-    <MemoryRouter initialEntries={[route]}>
-      <AuthProvider>{ui}</AuthProvider>
-    </MemoryRouter>
-  );
-}
+		render(
+			<MemoryRouter>
+				<CourseList />
+			</MemoryRouter>
+		);
 
-test('Testing if dropdown exists', async () => {
-  renderWithProviders(<CourseList />, { route: '/?division=MATH' });
-  expect(await screen.findByRole('combobox')).toBeInTheDocument();
-});
+		await waitFor(() => expect(axios.get).toHaveBeenCalledTimes(1));
 
-test('Render course list components', async () => {
-renderWithProviders(<CourseList />);
+    const element = document.getElementById('course-test-content');
+    
+    expect(element).toHaveTextContent("Course");
+    expect(element).toHaveTextContent("Title");
+    expect(element).toHaveTextContent("Instructor");
+    expect(element).toHaveTextContent("Email");
 
-await waitFor(() => {
-  expect(screen.getByText('List of Courses')).toBeInTheDocument();
-});
-
-await waitFor(() => {
-  expect(screen.getByText('Course')).toBeInTheDocument();
-  expect(screen.getByText('Title')).toBeInTheDocument();
-  expect(screen.getByText('Instructor')).toBeInTheDocument();
-  expect(screen.getByText('Email')).toBeInTheDocument();
-  });
-});
-
-
-test("Testing pagination exists", async () => {
-  renderWithProviders(<CourseList />);
-  expect(screen.getByText('<')).toBeInTheDocument();
-  expect(screen.getByText('>')).toBeInTheDocument();
+    expect(element).toHaveTextContent("MATH 100");
+    expect(element).toHaveTextContent("Differential Calculus with Applications to Physical Sciences and Engineering");
+    expect(element).toHaveTextContent("Brandi Floyd");
+    expect(element).toHaveTextContent("brandi@instructor.ubc.ca");
+	});
 });
