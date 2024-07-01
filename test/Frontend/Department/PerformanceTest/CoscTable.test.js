947ba834a2fc3465e1ff6f3feb80d2065a3b789e
Kevin Kim <rkdgus990503@gmail.com>
2024-07-01 15:48:04 -0700

    testing filter function by clicking year number 1

--- test/Frontend/Department/PerformanceTest/CoscTable.test.js
+++ test/Frontend/Department/PerformanceTest/CoscTable.test.js
@@ -4,6 +4,7 @@ import CoscTable from '../../../../app/frontend/src/JS/Department/PerformanceImp
 import {MemoryRouter} from "react-router-dom";
 import axios from 'axios';
 import { useAuth } from '../../../../app/frontend/src/JS/AuthContext';
+import userEvent from '@testing-library/user-event';
 
 jest.mock('axios');
 jest.mock('../../../../app/frontend/src/JS/AuthContext');
@@ -17,9 +18,9 @@ describe('CoscTable', () => {
 			Promise.resolve({
 				data: {
          courses: [{"courseCode" : "COSC 101", "rank":"A", "score":92},
-          {"courseCode" : "COSC 499", "rank":"D", "score":60},
-          {"courseCode" : "COSC 290", "rank":"D", "score":40},
-          {"courseCode" : "COSC 391", "rank":"D", "score":50},
+          {"courseCode" : "COSC 123", "rank":"C", "score":72},
+          {"courseCode" : "COSC 290", "rank":"D", "score":65},
+          {"courseCode" : "COSC 391", "rank":"D", "score":62},
          ] }
 			})
 		);
@@ -34,14 +35,38 @@ describe('CoscTable', () => {
 		await waitFor(() => expect(axios.get).toHaveBeenCalledTimes(1));
     const element = document.getElementById('cosc-test-content');
     expect(element).toHaveTextContent("COSC 101");
-    expect(element).toHaveTextContent("COSC 499");
+    expect(element).toHaveTextContent("COSC 123");
     expect(element).toHaveTextContent("COSC 290");
     expect(element).toHaveTextContent("COSC 391");
+
+    expect(element).toHaveTextContent("A");
+    expect(element).toHaveTextContent("C");
+    expect(element).toHaveTextContent("D");
+    expect(element).toHaveTextContent("D");
     
     expect(element).toHaveTextContent("92");
-    expect(element).toHaveTextContent("60");
-    expect(element).toHaveTextContent("40");
-    expect(element).toHaveTextContent("50");
+    expect(element).toHaveTextContent("72");
+    expect(element).toHaveTextContent("65");
+    expect(element).toHaveTextContent("62");
 
 	});
+
+  test('Testing filter courses by clicking year number 1', async () => {
+    const year1Button = screen.getByRole('button', { name: '1' });
+    userEvent.click(year1Button);
+
+    await waitFor(() => {
+      const rows = screen.getAllByRole('row');
+      expect(rows).toHaveLength(3);
+    })
+    const element = document.getElementById('cosc-test-content');
+    expect(element).toHaveTextContent('COSC 101');
+    expect(element).toHaveTextContent('A');
+    expect(element).toHaveTextContent('92');
+
+    expect(element).toHaveTextContent('COSC 123');
+    expect(element).toHaveTextContent('C');
+    expect(element).toHaveTextContent('72');
+  })
+  
 });
