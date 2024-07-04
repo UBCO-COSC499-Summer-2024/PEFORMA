041a5c3b023b6e00ecb7d67761747ecb0eb77e83
Kevin Kim <rkdgus990503@gmail.com>
2024-07-03 23:59:08 -0700

    sidebar

--- app/frontend/src/JS/sidebar.js
+++ app/frontend/src/JS/sidebar.js
@@ -4,17 +4,17 @@ import { Link } from 'react-router-dom';
 
 
 function Sidebar() { 
-  // const { profileId } = useAuth();
+   const { profileId } = useAuth();
     return(
       <aside className="sidebar">
       <h2>PEFORMA</h2>
       <hr className="divider" />
       <ul className="menu">
         <li><Link to={`http://localhost:3000/Dashboard?`}><FaHome className="icon" size={30} /> Dashboard</Link></li>
-        <li><Link to={`http://localhost:3000/PerformanceInstructorPage`}><FaChartLine className="icon" size={30} /> Performance</Link></li>
+        <li><Link to={`http://localhost:3000/PerformanceInstructorPage?profileId=${profileId}`}><FaChartLine className="icon" size={30} /> Performance</Link></li>
       </ul>
     </aside>
     );
 }
 export default Sidebar;
-//profileId=${profileId}
+//
