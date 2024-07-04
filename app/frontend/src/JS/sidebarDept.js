fa689fda09ae9bb06c4c29cb13886782b9571f20
Kevin Kim <rkdgus990503@gmail.com>
2024-07-03 23:25:28 -0700

    get rid of profileId from url for DataEntry

--- app/frontend/src/JS/sidebarDept.js
+++ app/frontend/src/JS/sidebarDept.js
@@ -12,7 +12,7 @@ function Sidebar() {
         <li><Link to={`http://localhost:3000/DeptDashboard?profileId=${profileId}`}><FaHome className="icon" size={30} /> Dashboard</Link></li>
         <li><Link to={``}><FaUser className="icon" size={30} /> People</Link></li>
         <li><Link to={`http://localhost:3000/ServiceRoleList?profileId=${profileId}`}><FaSuitcase className="icon" size={30} /> Service Roles</Link></li>
-        <li><Link to={`http://localhost:3000/DataEntry?profileId=${profileId}`}><FaPaperclip className="icon" size={30} /> Data Entry</Link></li>
+        <li><Link to={`http://localhost:3000/DataEntry`}><FaPaperclip className="icon" size={30} /> Data Entry</Link></li>
         <li><Link to={`http://localhost:3000/DeptCourseList?profileId=${profileId}`}><FaBook className="icon" size={30} /> Course</Link></li>
         <li><Link to={`http://localhost:3000/PerformanceDepartmentPage?profileId=${profileId}`}><FaChartLine className="icon" size={30} /> Performance</Link></li>
       </ul>
