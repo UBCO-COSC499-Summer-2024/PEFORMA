1721b290e05f9f4f4a1251e973133de4ca4c7b32
Kevin Kim <rkdgus990503@gmail.com>
2024-07-03 21:21:21 -0700

    added png for dashboard

--- app/frontend/src/JS/common/cardImages.js
+++ app/frontend/src/JS/common/cardImages.js
@@ -2,7 +2,8 @@ const cardImages = {
     COSC:require("../../images/Dashboard/green.png"),
     MATH:require("../../images/Dashboard/red.png"),
     PHYS:require("../../images/Dashboard/blue.png"),
-    STAT:require("../../images/Dashboard/yellow.png")
+    STAT:require("../../images/Dashboard/yellow.png"),
+    ALL:require("../../images/Dashboard/allColor.png"),
   };
 
   export default cardImages;
