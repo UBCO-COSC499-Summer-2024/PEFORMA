290223b240317c165cfc18f0de85e96566b82fca
ZhiHengZhang01 <77310385+ZhiHengZhang01@users.noreply.github.com>
2024-07-04 14:39:27 -0700

    move all existing FE tests

---
+++ app/frontend/test/All/Login.test.js
@@ -0,0 +1,29 @@
+import {render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import Login from '../../../app/frontend/src/JS/All/Login';
+import {MemoryRouter} from "react-router-dom";
+import { useAuth } from '../../../app/frontend/src/JS/AuthContext';
+import axios from 'axios';
+
+jest.mock('../../../app/frontend/src/JS/AuthContext');
+jest.mock('axios');
+
+useAuth.mockResolvedValue('MOCK');
+axios.get.mockResolvedValue(':)');
+axios.post.mockResolvedValue(":(");
+
+test('Check user input', async () => {
+    const user = userEvent.setup();
+    render(<MemoryRouter><Login/></MemoryRouter>);
+    const emailBox = screen.getByPlaceholderText('Email Address');
+    await user.type(emailBox, "Hello!");
+    expect(emailBox.value).toBe("Hello!");
+});
+
+test('Check login button exists', async () => {
+    const user = userEvent.setup();
+    render(<MemoryRouter><Login/></MemoryRouter>);
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument()
+});
