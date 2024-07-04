69266109f4c0077efb450beebd2f5f2c43369e80
fruitgushers <jeremyadams166@gmail.com>
2024-07-04 12:46:02 -0700

    Made performance department page look a little better, but can't get the top bar to be how I want it

--- app/frontend/src/JS/Department/PerformanceDepartmentPage.js
+++ app/frontend/src/JS/Department/PerformanceDepartmentPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import '../../CSS/Department/PerformanceDepartmentPage.css';
+
 import { CreateSidebarDept,CreateTopbar } from '../commonImports.js';
 import  CoscTable  from './PerformanceImports/CoscTable.js';
 import  MathTable  from './PerformanceImports/MathTable.js';
@@ -8,17 +8,19 @@ import  PhysTable  from './PerformanceImports/PhysTable.js';
 import  StatTable  from './PerformanceImports/StatTable.js';
 import GoodBadBoard from './PerformanceImports/GoodBadBoard.js';
 import BenchMark from './PerformanceImports/BenchMark.js';
+import '../../CSS/Department/PerformanceDepartmentPage.css';
 
 function PerformanceDepartmentPage() {
 
 	return (
-		<div className="dashboard-container">
+		<div className="dp-container">
 			<CreateSidebarDept />
 
 			<div className="container">
 				<CreateTopbar />
+				<div className='main'>
 				<div className="performanceD-title">
-          <h2>Department Performance Overview</h2>
+          <h1>Department Performance Overview</h1>
 				</div>
 
 				<div className="division-top-table">
@@ -45,15 +47,16 @@ function PerformanceDepartmentPage() {
 
         </div>
 
-				<div className="bottom-section">
-					<div className="benchmark-section">
+				<div className="bottom-sectin">
+					<div className="division">
 						<BenchMark />
 					</div>
 
-					<div className="people-section">
+					<div className="division">
 						<GoodBadBoard />
 					</div>
 					
+				</div>
 				</div>
 			</div>
 		</div>
