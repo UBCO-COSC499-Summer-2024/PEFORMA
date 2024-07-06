9e9440b8220d4cad0ca0282285f30a933863198c
Kevin Kim <rkdgus990503@gmail.com>
2024-07-05 19:10:50 -0700

    added another top search bar for member list

--- app/frontend/src/JS/common/topbarFilter.js
+++ app/frontend/src/JS/common/topbarFilter.js
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 
+// Instructor course list
 function TopSearchBarIns({ onSearch }) { 
     const navigate = useNavigate();
     const handleLogOut = () => {
@@ -7,14 +8,15 @@ function TopSearchBarIns({ onSearch }) {
         alert('Log out successfully.\n\nRedirecting to Home Page.');
         navigate('/');
     }
-        return(
-            <header className="topbar-search">
-                <input type="text" placeholder="Search by Subject (e.g. COSC 111), Title (e.g. Computer Programming I), Instructor (e.g. John Doe)" onChange={e => onSearch(e.target.value)} />
-                <div className="logout" onClick={handleLogOut}>Logout</div>
-            </header>
-        );
-    }
+    return(
+        <div className="topbar-search">
+            <input type="text" placeholder="Search by Subject (e.g. COSC 111), Title (e.g. Computer Programming I), Instructor (e.g. John Doe)" onChange={e => onSearch(e.target.value)} />
+            <div className="logout" onClick={handleLogOut}>Logout</div>
+        </div>
+    );
+}
 
+// Department course list
 function TopSearchBarDept({ onSearch }) {
     const navigate = useNavigate();
     const handleLogOut = () => {
@@ -24,10 +26,25 @@ function TopSearchBarDept({ onSearch }) {
     }
     return (
         <div className="topbar-search">
-        <input type="text" placeholder="Search by Subject (e.g. COSC 111), Title (e.g. Computer Programming I)" onChange={e => onSearch(e.target.value)} />
-        <div className="logout" onClick={handleLogOut}>Logout</div>
-
+            <input type="text" placeholder="Search by Subject (e.g. COSC 111), Title (e.g. Computer Programming I)" onChange={e => onSearch(e.target.value)} />
+            <div className="logout" onClick={handleLogOut}>Logout</div>
         </div>
     );
 }
-export {TopSearchBarIns, TopSearchBarDept};
+
+// Instructor member list
+function TopSearchBarMemberList({ onSearch }) {
+    const navigate = useNavigate();
+    const handleLogOut = () => {
+        localStorage.removeItem('token');
+        alert('Log out successfully.\n\nRedirecting to Home Page.');
+        navigate('/');
+    }
+    return (
+        <div className="topbar-search">
+            <input type="text" placeholder="Search by Name (e.g. John Doe), Service Role (e.g. Advisor)" onChange={e => onSearch(e.target.value)} />
+            <div className="logout" onClick={handleLogOut}>Logout</div>
+        </div>
+    );
+}
+export {TopSearchBarIns, TopSearchBarDept, TopSearchBarMemberList};
