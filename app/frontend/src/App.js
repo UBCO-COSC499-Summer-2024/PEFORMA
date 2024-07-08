bc11a137046dee31ad451424e5f4059685b6beac
Kevin Kim <rkdgus990503@gmail.com>
2024-07-08 13:58:15 -0700

    fix route in app.js not showing ins edit profile

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -52,7 +52,7 @@ function App() {
 						}
 					/>
 					<Route path="/InsCourseList" element={<InsCourseList />} />
-					<Route path="/EditProfile" element={<InsEditProfile />} />
+					<Route path="/InsEditProfile" element={<InsEditProfile />} />
 					<Route path="/InsPerformancePage" element={<InsPerformancePage />} />
 					<Route path="/InsProfilePage" element={<InsProfilePage />} />
 					<Route path="/InsCourseHistory" element={<InsCourseHistory />} />
