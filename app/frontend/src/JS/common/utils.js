dc18fb216521ef51ff4b399cbef50a6fdb8d2174
Kevin Kim <rkdgus990503@gmail.com>
2024-07-08 21:15:15 -0700

    refactored checkAccess to be reusable in all files

--- app/frontend/src/JS/common/utils.js
+++ app/frontend/src/JS/common/utils.js
@@ -31,3 +31,17 @@ export const currentItems = (items, currentPage, itemsPerPage) => {
 export const handleSearchChange = (setStateFunction) => {
   setStateFunction(prevState => ({ ...prevState, currentPage: 1 }));
 };
+
+export const checkAccess = (accountLogInType, navigate, currentView) => {
+  const numericAccountType = Number(accountLogInType);
+
+  // if your accLogInType is 1 or 2 = dept view only and trying to access Instructor? deny access
+  if ((numericAccountType === 1 || numericAccountType === 2) && currentView === 'instructor') {
+      alert('No Access, Redirecting to department view');
+      navigate('/DeptDashboard');
+  // if your accLogInType is 3 = instructor view only and trying to access department? deny access
+  } else if (numericAccountType === 3 && currentView === 'department') {
+      alert('No Access, Redirecting to instructor view');
+      navigate('/InsDashboard');
+  }
+}
