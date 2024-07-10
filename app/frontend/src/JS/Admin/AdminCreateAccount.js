7ada3b98c208567f962ba2e32031ab9fd1a8296b
Kevin Kim <rkdgus990503@gmail.com>
2024-07-10 15:48:37 -0700

    styling create account

--- app/frontend/src/JS/Admin/AdminCreateAccount.js
+++ app/frontend/src/JS/Admin/AdminCreateAccount.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import '../../CSS/Admin/CreateAccount.css';
 import axios from 'axios';
+import CreateSideBar, { CreateTopBar } from '../common/commonImports.js';
 
 function CreateAccount() {
     const currentYear = new Date().getFullYear();
@@ -65,41 +66,50 @@ function CreateAccount() {
     const handleAssign = () => setShowRoleModal(true);
 
     return (
-        <div className="create-account-form">
-            <h1>Create Account</h1>
-            <form onSubmit={handleSubmit}>
-                <input type="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange} />
-                <input type="text" name="firstName" placeholder="First Name" value={formData.firstName} onChange={handleChange} />
-                <input type="text" name="lastName" placeholder="Last Name" value={formData.lastName} onChange={handleChange} />
-                <input type="text" name="ubcId" placeholder="UBC ID" value={formData.ubcId} onChange={handleChange} />
-                <div style={{display:'flex'}}>
-                <input type="text" name="serviceRole" placeholder="Service Role" value={formData.serviceRole} readOnly />
-                <button type="button" onClick={handleAssign}>Assign Role(s)</button>
-                </div>
-                <input type='text' name="year" placeholder="Assigned Year" value={formData.year} onChange={handleChange}/>
-                <input type="integer" name="division" placeholder="Division" value={formData.division} onChange={handleChange} />
-                <input type="password" name="password" placeholder="Password" value={formData.password} onChange={handleChange} />
-                <input type="password" name="confirmPassword" placeholder="Confirm Password" value={formData.confirmPassword} onChange={handleChange} />
-                <button type="submit" className="create-button">Create</button>
-                <button type="button" className="cancel-button" onClick={handleCancel}>Cancel</button>
-            </form>
-
-            {showRoleModal && (
-                <div className="modal-overlay">
-                    <div className="modal-content">
-                        <button onClick={() => setShowRoleModal(false)}>Close</button>
-                        {roles.map(role => (
-                            <div key={role.id}>
-                                {role.name}
-                                <button onClick={() => toggleRoleAdded(role.id)}>
-                                    {role.added ? 'Remove' : 'Add'}
-                                </button>
+        <div className="dashboard">
+            <CreateSideBar sideBarType="Admin" />
+            <div className='container'>
+            <CreateTopBar/>
+            <div className='create-account-form'>
+                <h1>Create Account</h1>
+                <form onSubmit={handleSubmit}>
+                    <input type="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange} />
+                    <input type="text" name="firstName" placeholder="First Name" value={formData.firstName} onChange={handleChange} />
+                    <input type="text" name="lastName" placeholder="Last Name" value={formData.lastName} onChange={handleChange} />
+                    <input type="text" name="ubcId" placeholder="UBC ID" value={formData.ubcId} onChange={handleChange} />
+                    <div className='service-role-button-align'>
+                        <input type="text" name="serviceRole" placeholder="Service Role" value={formData.serviceRole} readOnly />
+                        <button type="button" onClick={handleAssign}>Assign Role(s)</button>
+                        {showRoleModal && (
+                            <div className="modal-overlay">
+                                <div className="modal-content">
+                                    <button onClick={() => setShowRoleModal(false)}>Close</button>
+                                    {roles.map(role => (
+                                        <div key={role.id}>
+                                            {role.name}
+                                            <button onClick={() => toggleRoleAdded(role.id)}>
+                                                {role.added ? 'Remove' : 'Add'}
+                                            </button>
+                                        </div>
+                                    ))}
+                                    <button onClick={() => setShowRoleModal(false)}>Save</button>
+                                </div>
                             </div>
-                        ))}
-                        <button onClick={() => setShowRoleModal(false)}>Save</button>
+                        )}
                     </div>
+                    <input type='text' name="year" placeholder="Assigned Year" value={formData.year} onChange={handleChange}/>
+                    <input type="integer" name="division" placeholder="Division" value={formData.division} onChange={handleChange} />
+                    <input type="password" name="password" placeholder="Password" value={formData.password} onChange={handleChange} />
+                    <input type="password" name="confirmPassword" placeholder="Confirm Password" value={formData.confirmPassword} onChange={handleChange} />
+                    <div className='button-align-div'>
+                        <button type="submit" className="create-button">Create</button>
+                        <button type="button" className="cancel-button" onClick={handleCancel}>Cancel</button>
+                    </div>
+                </form>
+
+                
                 </div>
-            )}
+            </div>
         </div>
     );
 }
