ccce6103561904747476fa97858b6e1840db22f9
Kevin Kim <rkdgus990503@gmail.com>
2024-07-10 16:35:23 -0700

    fixing create account

--- app/frontend/src/JS/Admin/AdminCreateAccount.js
+++ app/frontend/src/JS/Admin/AdminCreateAccount.js
@@ -1,9 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../../CSS/Admin/CreateAccount.css';
 import axios from 'axios';
 import CreateSideBar, { CreateTopBar } from '../common/commonImports.js';
+import { useAuth } from '../common/AuthContext.js';
+import { useNavigate } from 'react-router-dom';
+import { fillEmptyItems } from '../common/utils.js';
 
 function CreateAccount() {
+    const { authToken, accountLogInType } = useAuth();
+    const navigate = useNavigate();
+    const [roleData, setRoleData] = useState({
+        roles: [{}],
+        rolesCount: 0,
+        perPage: 10,
+        currentPage: 1,
+    });
     const currentYear = new Date().getFullYear();
     const [formData, setFormData] = useState({
         email: '',
@@ -11,26 +22,66 @@ function CreateAccount() {
         lastName: '',
         ubcId: '',
         serviceRole: '',
-        year : '',
+        year: '',
         division: '',
         password: '',
         confirmPassword: ''
     });
-    
+
+    const [roles, setRoles] = useState([]);
     const [showRoleModal, setShowRoleModal] = useState(false);
-    const [roles, setRoles] = useState([
-        { id : 1, name: 'Undergraduate Advisor', added: false, year: currentYear },
-        { id : 2, name: 'Graduate Admissions', added: false, year: currentYear },
-    ]);
+
+    useEffect(() => {
+        const fetchServiceRoles = async () => {
+            try {
+                if (!authToken) {
+                    navigate('/Login'); // Use your navigation mechanism
+                    return;
+                }
+
+                const res = await axios.get(`http://localhost:3001/api/service-roles`, {
+                    headers: { Authorization: `Bearer ${authToken.token}` },
+                });
+                const data = res.data;
+                const filledRoles = fillEmptyItems(data.roles, data.perPage);
+                setRoleData({ ...data, roles: filledRoles });
+            } catch (error) {
+                if (error.response && error.response.status === 401) {
+                    localStorage.removeItem('authToken'); // Clear invalid token
+                    navigate('/Login');
+                } else {
+                    console.error('Error fetching service roles:', error);
+                }
+            }
+        };
+        fetchServiceRoles();
+    }, [authToken]);
+
+    useEffect(() => {
+        const fetchRoles = async () => {
+            try {
+                const res = await axios.get(`http://localhost:3001/api/service-roles`, {
+                    headers: { Authorization: `Bearer ${authToken.token}` },
+                });
+                setRoles(res.data.roles);
+            } catch (error) {
+                console.error('Error fetching roles:', error);
+            }
+        };
+        if (showRoleModal) {
+            fetchRoles();
+        }
+    }, [showRoleModal, authToken]);
+
     const toggleRoleAdded = (roleId) => {
-        const updatedRoles = roles.map(role => 
-            ({ ...role, added: role.id === roleId ? !role.added : false })
+        const updatedRoles = roles.map(role =>
+            ({ ...role, added: role.id === roleId ? !role.added : role.added })
         );
         setRoles(updatedRoles);
-    
+
         const addedRole = updatedRoles.find(role => role.added);
-        setFormData({ 
-            ...formData, 
+        setFormData({
+            ...formData,
             serviceRole: addedRole ? [addedRole.name] : []
         });
     };
@@ -49,7 +100,7 @@ function CreateAccount() {
 
         const postData = {
             ...formData,
-            serviceRole: formData.serviceRole,  // This is now an array of role IDs
+            serviceRole: formData.serviceRole,
         };
         try {
             const response = await axios.post('http://localhost:3001/create-account', postData);
@@ -69,45 +120,43 @@ function CreateAccount() {
         <div className="dashboard">
             <CreateSideBar sideBarType="Admin" />
             <div className='container'>
-            <CreateTopBar/>
-            <div className='create-account-form'>
-                <h1>Create Account</h1>
-                <form onSubmit={handleSubmit}>
-                    <input type="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange} />
-                    <input type="text" name="firstName" placeholder="First Name" value={formData.firstName} onChange={handleChange} />
-                    <input type="text" name="lastName" placeholder="Last Name" value={formData.lastName} onChange={handleChange} />
-                    <input type="text" name="ubcId" placeholder="UBC ID" value={formData.ubcId} onChange={handleChange} />
-                    <div className='service-role-button-align'>
-                        <input type="text" name="serviceRole" placeholder="Service Role" value={formData.serviceRole} readOnly />
-                        <button type="button" onClick={handleAssign}>Assign Role(s)</button>
-                        {showRoleModal && (
-                            <div className="modal-overlay">
-                                <div className="modal-content">
-                                    <button onClick={() => setShowRoleModal(false)}>Close</button>
-                                    {roles.map(role => (
-                                        <div key={role.id}>
-                                            {role.name}
-                                            <button onClick={() => toggleRoleAdded(role.id)}>
-                                                {role.added ? 'Remove' : 'Add'}
-                                            </button>
-                                        </div>
-                                    ))}
-                                    <button onClick={() => setShowRoleModal(false)}>Save</button>
+                <CreateTopBar />
+                <div className='create-account-form'>
+                    <h1>Create Account</h1>
+                    <form onSubmit={handleSubmit}>
+                        <input type="email" name="email" placeholder="Email" value={formData.email} onChange={handleChange} required />
+                        <input type="text" name="firstName" placeholder="First Name" value={formData.firstName} onChange={handleChange} required />
+                        <input type="text" name="lastName" placeholder="Last Name" value={formData.lastName} onChange={handleChange} required />
+                        <input type="text" name="ubcId" placeholder="UBC ID (optional)" value={formData.ubcId} onChange={handleChange} required minLength="8" maxLength="8" />
+                        <div className='service-role-button-align'>
+                            <input type="text" name="serviceRole" placeholder="Service Role" value={formData.serviceRole} readOnly />
+                            <button type="button" onClick={handleAssign}>Assign Role(s)</button>
+                            {showRoleModal && (
+                                <div className="modal-overlay">
+                                    <div className="modal-content">
+                                        <button onClick={() => setShowRoleModal(false)}>Close</button>
+                                        {roles.map(role => (
+                                            <div key={role.id}>
+                                                {role.name}
+                                                <button onClick={() => toggleRoleAdded(role.id)}>
+                                                    {role.added ? 'Remove' : 'Add'}
+                                                </button>
+                                            </div>
+                                        ))}
+                                        <button onClick={() => setShowRoleModal(false)}>Save</button>
+                                    </div>
                                 </div>
-                            </div>
-                        )}
-                    </div>
-                    <input type='text' name="year" placeholder="Assigned Year" value={formData.year} onChange={handleChange}/>
-                    <input type="integer" name="division" placeholder="Division" value={formData.division} onChange={handleChange} />
-                    <input type="password" name="password" placeholder="Password" value={formData.password} onChange={handleChange} />
-                    <input type="password" name="confirmPassword" placeholder="Confirm Password" value={formData.confirmPassword} onChange={handleChange} />
-                    <div className='button-align-div'>
-                        <button type="submit" className="create-button">Create</button>
-                        <button type="button" className="cancel-button" onClick={handleCancel}>Cancel</button>
-                    </div>
-                </form>
-
-                
+                            )}
+                        </div>
+                        <input type='number' name="year" placeholder="Assigned Year" value={formData.year} onChange={handleChange} required/>
+                        <input type="integer" name="division" placeholder="Division" value={formData.division} onChange={handleChange} required />
+                        <input type="password" name="password" placeholder="Password" value={formData.password} onChange={handleChange} required />
+                        <input type="password" name="confirmPassword" placeholder="Confirm Password" value={formData.confirmPassword} onChange={handleChange} required />
+                        <div className='button-align-div'>
+                            <button type="submit" className="create-button">Create</button>
+                            <button type="button" className="cancel-button" onClick={handleCancel}>Cancel</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
