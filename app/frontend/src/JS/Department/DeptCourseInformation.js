6a038b8e24af68ee9ee338fb8cb1fb67dc29696b
fruitgushers <jeremyadams166@gmail.com>
2024-07-17 13:37:05 -0700

    Made necessary changes

--- app/frontend/src/JS/Department/DeptCourseInformation.js
+++ app/frontend/src/JS/Department/DeptCourseInformation.js
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { useAuth } from '../common/AuthContext.js';
 import { useNavigate } from 'react-router-dom';
+import { getCurrentInstructor } from '../common/utils.js';
 
 function CourseInformation() {
   const { authToken, accountLogInType } = useAuth();
@@ -26,6 +27,8 @@ function CourseInformation() {
     avgScore: '',
     currentInstructor: 'Willem Dafoe'
   });
+  const [currentInstructor, setCurrentInstructor] = useState([]);
+  const [numInstructors, setNumInstructors] = useState(0);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -47,6 +50,13 @@ function CourseInformation() {
         const filledEntries = fillEmptyEntries(data.history, data.perPage);
         setCourseData({ ...data, history: filledEntries });
         setEditDescription(data.courseDescription);
+		setCurrentInstructor(getCurrentInstructor(data));
+		if (data.history[0].instructorID == null || data.history[0].instructorID == "") {
+			setNumInstructors(0);
+		} else {
+			setNumInstructors(data.history.length);
+		}
+
       } catch (error) {
         console.error('Error fetching data:', error);
       }
@@ -130,10 +140,29 @@ function CourseInformation() {
               <p style={{ whiteSpace: 'pre-wrap' }}>{courseData.courseDescription}</p>
             )}
           </div>
-          <p id="current-instructor">Current Instructor: {courseData.currentInstructor}</p>
+          
           <div className="bold score">
             Average Performance Score: <span role="contentinfo">{courseData.avgScore}</span>
           </div>
+		  <div className="current-instructor">
+						<p>Current Instructor(s): {currentInstructor.length === 0 && (
+							<strong>N/A</strong>
+						)}
+						{currentInstructor.length !== 0 && (
+							currentInstructor.map((instructor, index) => {
+								return (
+								<span>
+								<Link to={"/DeptProfilePage?ubcid="+instructor.instructorID}><strong>{instructor.instructorName}</strong></Link>
+								{index !== currentInstructor.length - 1 && (
+									<span>, </span>
+								)}
+								</span>
+								);
+
+							})
+						)}
+							</p>
+					</div>
           <div className="buttons">
             {isEditing ? (
               <button role="button" onClick={handleSaveClick}>
@@ -146,7 +175,7 @@ function CourseInformation() {
             )}
           </div>
           <div id="history">
-            <p className="bold">Course History</p>
+		  <p className="bold">Course History ({numInstructors} Entries)</p>
             <table id="historyTable">
               <thead>
                 <tr>
@@ -196,190 +225,3 @@ function CourseInformation() {
 }
 
 export default CourseInformation;
-
-
-/*
-
-import React, { useState, useEffect } from 'react';
-import CreateSideBar from '../common/commonImports.js';
-import { CreateTopBar } from '../common/commonImports.js';
-import ReactPaginate from 'react-paginate';
-import '../../CSS/Department/DeptCourseInformation.css';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
-import { useAuth } from '../common/AuthContext.js';
-import { useNavigate } from 'react-router-dom';
-
-function CourseInformation() {
-  const { authToken, accountLogInType } = useAuth();
-  const navigate = useNavigate();
-  const params = new URLSearchParams(window.location.search);
-  const courseId = params.get('courseid');
-  const [isEditing, setIsEditing] = useState(false);
-  const [editDescription, setEditDescription] = useState('');
-  const [courseData, setCourseData] = useState({
-    history: [{}],
-    entryCount: 0,
-    perPage: 10,
-    currentPage: 1,
-    courseCode: '',
-    courseName: '',
-    courseDescription: '',
-    avgScore: ''
-  });
-
-  useEffect(() => {
-    console.log('Use effect executing');
-    const fetchData = async () => {
-      if (!authToken) {
-        navigate('/Login');
-        return;
-      }
-      const numericAccountType = Number(accountLogInType);
-      if (numericAccountType !== 1 && numericAccountType !== 2) {
-        alert('No Access, Redirecting to instructor view');
-        navigate('/Dashboard');
-      }
-      console.log('Before fetch');
-      const res = await axios.get(`http://localhost:3001/api/courseHistory`, {
-        params: { courseId: courseId },
-        headers: { Authorization: `Bearer ${authToken.token}` },
-      });
-      console.log('After res');
-      const data = res.data;
-      setCourseData({ ...courseData, history: data.history, entryCount: data.history.length });
-      setEditDescription(data.courseDescription);
-    };
-    fetchData();
-  }, [authToken, accountLogInType, navigate, courseId]);
-
-  const handleEditClick = () => {
-    setIsEditing(true);
-  };
-
-  const handleSaveClick = async () => {
-    setIsEditing(false);
-    const updatedCourseData = { courseId, courseDescription: editDescription };
-    console.log('Updated data:\n', JSON.stringify(updatedCourseData));
-    try {
-      const res = await axios.post('http://localhost:3001/api/updateCourseInfo', updatedCourseData, {
-        headers: { Authorization: `Bearer ${authToken.token}` },
-      });
-      console.log('Update successful', res.data);
-      window.location.reload(); // Refresh the page after successful save
-    } catch (error) {
-      console.error('Error updating course info', error);
-    }
-  };
-
-  const handleChange = (e) => {
-    const { value } = e.target;
-    setEditDescription(value);
-    e.target.style.height = 'auto';
-    e.target.style.height = e.target.scrollHeight + 'px';
-  };
-
-  const handlePageClick = (data) => {
-    setCourseData((prevState) => ({
-      ...prevState,
-      currentPage: data.selected + 1,
-    }));
-  };
-
-  const pageCount = Math.ceil(courseData.entryCount / courseData.perPage);
-  const offset = (courseData.currentPage - 1) * courseData.perPage;
-
-  const currentEntries = courseData.history.slice(
-    (courseData.currentPage - 1) * courseData.perPage,
-    courseData.currentPage * courseData.perPage
-  );
-
-  let i = 0;
-
-  return (
-    <div className="dashboard coursehistory">
-      <CreateSideBar sideBarType="Department" />
-      <div className="container">
-        <CreateTopBar />
-        <div className="courseinfo-main">
-          <Link to="/DeptCourseList">&lt; Back to All Courses</Link>
-          <h1 className="courseName" role="contentinfo">
-            {courseData.courseCode}: {courseData.courseName}
-          </h1>
-          <div className="description" style={{ whiteSpace: isEditing ? 'pre-wrap' : '' }}>
-            {isEditing ? (
-              <textarea
-                name="courseDescription"
-                value={editDescription}
-                onChange={handleChange}
-                className="editable-textarea"
-                style={{ minHeight: '80px', height: 'auto', overflow: 'hidden' }}
-              />
-            ) : (
-              <p style={{ whiteSpace: 'pre-wrap' }}>{courseData.courseDescription}</p>
-            )}
-          </div>
-          <div className="bold score">
-            Average Performance Score: <span role="contentinfo">{courseData.avgScore}</span>
-          </div>
-          <div className="buttons">
-            {isEditing ? (
-              <button role="button" onClick={handleSaveClick}>
-                Save
-              </button>
-            ) : (
-              <button id="edit" role="button" onClick={handleEditClick}>
-                Edit Course
-              </button>
-            )}
-          </div>
-          <div id="history">
-            <p className="bold">Course History</p>
-            <table id="historyTable">
-              <thead>
-                <tr>
-                  <th>Instructor</th>
-                  <th>Year & Term</th>
-                  <th>Performance Score</th>
-                </tr>
-              </thead>
-              <tbody>
-                {currentEntries.map((entry) => {
-                  i++;
-                  return (
-                    <tr key={i}>
-                      <td>
-                        <Link to={`/DeptProfilePage?ubcid=${entry.instructorID}`}>{entry.instructorName}</Link>
-                      </td>
-                      <td>{entry.term}</td>
-                      <td>{entry.score}</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-              <tfoot>
-                <tr>
-                  <td colSpan={3}>
-                    <ReactPaginate
-                      previousLabel={'<'}
-                      nextLabel={'>'}
-                      breakLabel={'...'}
-                      pageCount={pageCount}
-                      marginPagesDisplayed={3}
-                      pageRangeDisplayed={0}
-                      onPageChange={handlePageClick}
-                      containerClassName={'pagination'}
-                    />
-                  </td>
-                </tr>
-              </tfoot>
-            </table>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-export default CourseInformation;
-*/
