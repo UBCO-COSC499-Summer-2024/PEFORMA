9c61ab9747bd399e2b4ade6faacf69b998dd43f9
Kevin Kim <rkdgus990503@gmail.com>
2024-07-17 14:21:40 -0700

    fixed testing based on new feature on creatAccount

--- test/Frontend/Admin/AdminCreateAccount.test.js
+++ test/Frontend/Admin/AdminCreateAccount.test.js
@@ -1,7 +1,7 @@
 import { fireEvent, render, waitFor, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import AdminCreateAccount from '../../../app/frontend/src/JS/Admin/AdminCreateAccount';
-import { MemoryRouter, useLocation } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import axios from 'axios';
 import { useAuth } from '../../../app/frontend/src/JS/common/AuthContext';
 
@@ -29,31 +29,50 @@ describe('AdminCreateAccount', () => {
     expect(element).toContainElement(screen.getByPlaceholderText('Last Name'));
     expect(element).toContainElement(screen.getByPlaceholderText('UBC ID (optional)'));
     expect(element).toContainElement(screen.getByText('Select Department'));
+    expect(element).toContainElement(screen.getByLabelText('Department Head'));
+    expect(element).toContainElement(screen.getByLabelText('Department Staff'));
+    expect(element).toContainElement(screen.getByLabelText('Instructor'));
+    expect(element).toContainElement(screen.getByLabelText('Admin'));
     expect(element).toContainElement(screen.getByPlaceholderText('Password'));
     expect(element).toContainElement(screen.getByPlaceholderText('Confirm Password'));
-    
-  });
+});
   test('Testing form input changes', () => {
     fireEvent.change(element.querySelector('input[name="email"]'), { target: { value: 'testing@gamil.com' } });
     fireEvent.change(element.querySelector('input[name="firstName"]'), { target: { value: 'Kevin' } });
     fireEvent.change(element.querySelector('input[name="lastName"]'), { target: { value: 'Kim' } });
     fireEvent.change(element.querySelector('input[name="ubcId"]'), { target: { value: '25347922' } });
     fireEvent.change(element.querySelector('select[name="division"]'), { target: { value: '1' } }); 
+    fireEvent.click(element.querySelector('input[name="DepartmentHead"]'));
+    fireEvent.click(element.querySelector('input[name="Instructor"]'));
+    fireEvent.click(element.querySelector('input[name="Admin"]'));
     fireEvent.change(element.querySelector('input[name="password"]'), { target: { value: 'qwer1@3$' } });
     fireEvent.change(element.querySelector('input[name="confirmPassword"]'), { target: { value: 'qwer1@3$' } });
-
 
     expect(element.querySelector('input[name="email"]').value).toBe('testing@gamil.com');
     expect(element.querySelector('input[name="firstName"]').value).toBe('Kevin');
     expect(element.querySelector('input[name="lastName"]').value).toBe('Kim');
     expect(element.querySelector('input[name="ubcId"]').value).toBe('25347922');
     expect(element.querySelector('select[name="division"]').value).toBe('1'); 
+    expect(element.querySelector('input[name="DepartmentHead"]').checked).toBe(true);
+    expect(element.querySelector('input[name="DepartmentStaff"]').checked).toBe(false);
+    expect(element.querySelector('input[name="Instructor"]').checked).toBe(true);
+    expect(element.querySelector('input[name="Admin"]').checked).toBe(true);
     expect(element.querySelector('input[name="password"]').value).toBe('qwer1@3$');
     expect(element.querySelector('input[name="confirmPassword"]').value).toBe('qwer1@3$');
   });
+  test('Testing accountType checkbox changes', () => {
+    fireEvent.click(element.querySelector('input[name="DepartmentHead"]'));
+    fireEvent.click(element.querySelector('input[name="Admin"]'));
+
+    expect(element.querySelector('input[name="DepartmentHead"]').checked).toBe(true); // clicked checkbox, so true
+    expect(element.querySelector('input[name="DepartmentStaff"]').checked).toBe(false);
+    expect(element.querySelector('input[name="Instructor"]').checked).not.toBe(true); //didnt check Instructor checkbox so should be False
+    expect(element.querySelector('input[name="Admin"]').checked).toBe(true); // clicked checkbox, so true
+  });
+
   test('Testing password matches', async () => {
     window.alert = jest.fn(); 
-  
+
     fireEvent.change(element.querySelector('input[name="email"]'), { target: { value: 'random@gmail.com' } });
     fireEvent.change(element.querySelector('input[name="firstName"]'), { target: { value: 'random' } });
     fireEvent.change(element.querySelector('input[name="lastName"]'), { target: { value: 'guy' } });
@@ -61,9 +80,9 @@ describe('AdminCreateAccount', () => {
     fireEvent.change(element.querySelector('select[name="division"]'), { target: { value: '2' } });
     fireEvent.change(element.querySelector('input[name="password"]'), { target: { value: 'pathworld' } });
     fireEvent.change(element.querySelector('input[name="confirmPassword"]'), { target: { value: 'wrongPassWord@' } });
-  
+
     fireEvent.submit(element.querySelector('form'));
-  
+
     await waitFor(() => expect(window.alert).toHaveBeenCalledWith('Passwords do not match.'));
     expect(axios.post).not.toHaveBeenCalled(); // password do not match, no calling axios.post
   });
@@ -75,6 +94,8 @@ describe('AdminCreateAccount', () => {
     fireEvent.change(element.querySelector('input[name="lastName"]'), { target: { value: 'Oh' } });
     fireEvent.change(element.querySelector('input[name="ubcId"]'), { target: { value: '55232312' } });
     fireEvent.change(element.querySelector('select[name="division"]'), { target: { value: '3' } });
+    fireEvent.click(element.querySelector('input[name="DepartmentHead"]'));
+    fireEvent.click(element.querySelector('input[name="Instructor"]'));
     fireEvent.change(element.querySelector('input[name="password"]'), { target: { value: 'rand0mpassword1@' } });
     fireEvent.change(element.querySelector('input[name="confirmPassword"]'), { target: { value: 'rand0mpassword1@' } });
 
@@ -89,26 +110,43 @@ describe('AdminCreateAccount', () => {
         lastName: 'Oh',
         ubcId: '55232312',
         division: '3',
+        accountType: [1, 3], 
         password: 'rand0mpassword1@',
         confirmPassword: 'rand0mpassword1@'
       },
       { headers: { Authorization: `Bearer mocked-token` } }
     ));
 
-    await waitFor(() =>
-      expect(element.querySelector('input[name="email"]').value).toBe(''),
-      expect(element.querySelector('input[name="email"]').value).not.toBe('Brian')
-    );
+    await waitFor(() => {
+      expect(element.querySelector('input[name="email"]').value).toBe('');
+      expect(element.querySelector('input[name="firstName"]').value).toBe('');
+      expect(element.querySelector('input[name="lastName"]').value).toBe('');
+      expect(element.querySelector('input[name="ubcId"]').value).toBe('');
+      expect(element.querySelector('select[name="division"]').value).toBe('');
+      expect(element.querySelector('input[name="password"]').value).toBe('');
+      expect(element.querySelector('input[name="confirmPassword"]').value).toBe('');
+      expect(element.querySelector('input[name="DepartmentHead"]').checked).toBe(false);
+      expect(element.querySelector('input[name="DepartmentStaff"]').checked).toBe(false);
+      expect(element.querySelector('input[name="Instructor"]').checked).toBe(false);
+      expect(element.querySelector('input[name="Admin"]').checked).toBe(false);
+    });
   });
   test('Testing cancel button will reset form', () => {
     fireEvent.change(element.querySelector('input[name="email"]'), { target: { value: 'test@example.com' } });
     fireEvent.change(element.querySelector('input[name="firstName"]'), { target: { value: 'random' } });
     fireEvent.change(element.querySelector('input[name="lastName"]'), { target: { value: 'guy' } });
+    fireEvent.click(element.querySelector('input[name="Instructor"]'));
+    fireEvent.click(element.querySelector('input[name="Admin"]'));
+
+    expect(element.querySelector('input[name="Instructor"]').checked).toBe(true);
+    expect(element.querySelector('input[name="Admin"]').checked).toBe(true);
 
     fireEvent.click(screen.getByText('Cancel'));
 
     expect(element.querySelector('input[name="email"]').value).toBe('');
     expect(element.querySelector('input[name="firstName"]').value).toBe('');
     expect(element.querySelector('input[name="lastName"]').value).toBe('');
+    expect(element.querySelector('input[name="Instructor"]').checked).toBe(false);
+    expect(element.querySelector('input[name="Admin"]').checked).toBe(false);
   });
 });
