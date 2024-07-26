6a65a747d5350ada4e8be8185331006c18130579
subarusakashita <77289992+subarusakashita@users.noreply.github.com>
2024-07-26 17:12:40 +0900

    Update teachingAssignment.js

--- app/backend/services/teachingAssignment.js
+++ app/backend/services/teachingAssignment.js
@@ -4,20 +4,21 @@ async function getTeachingAssignment() {
   try {
     const currTerm = await getLatestTerm();
     result = await pool.query(`
-    SELECT 
-    TRIM(p."firstName" || ' ' || COALESCE(p."middleName" || ' ', '') || p."lastName") AS full_name,
-    p."UBCId", 
-    d."dname" AS department_name,
+   SELECT 
+    COALESCE(TRIM(p."firstName" || ' ' || COALESCE(p."middleName" || ' ', '') || p."lastName"), 'Not Assigned') AS full_name,
+    COALESCE(p."UBCId", 'N/A') AS "UBCId", 
+    COALESCE(d."dname", 'N/A') AS department_name,
     COALESCE(ARRAY_AGG(DISTINCT c2."dcode" || ' ' || c."courseNum") FILTER (WHERE c."courseNum" IS NOT NULL), '{}') AS courses,
     COALESCE(ARRAY_AGG(DISTINCT c."ctitle") FILTER (WHERE c."ctitle" IS NOT NULL), '{}') AS courseName,
     COALESCE(ARRAY_AGG(DISTINCT c."courseId") FILTER (WHERE c."courseId" IS NOT NULL), '{}') AS courseId,
-    p."email"
-    FROM "Profile" p
+    COALESCE(p."email", 'N/A') AS email
+    FROM "CourseByTerm" cbt
+    LEFT JOIN "Course" c ON c."courseId" = cbt."courseId"
+    LEFT JOIN "Division" c2 ON c2."divisionId" = c."divisionId"
+    LEFT JOIN "InstructorTeachingAssignment" ita ON ita."courseId" = cbt."courseId" AND ita."term" = cbt."term"
+    LEFT JOIN "Profile" p ON p."profileId" = ita."profileId"
     LEFT JOIN "Division" d ON d."divisionId" = p."divisionId"
-    LEFT JOIN "InstructorTeachingAssignment" ita ON ita."profileId" = p."profileId"
-    LEFT JOIN "Course" c ON c."courseId" = ita."courseId" 
-    LEFT JOIN "Division" c2 ON c2."divisionId" = c."divisionId" 
-    WHERE ita."term" = $1
+    WHERE cbt."term" = $1
     GROUP BY p."firstName", p."middleName", p."lastName", p."email", p."UBCId", d."dname"
     `, [currTerm]);
     // Reformat the data
