07424695081622ee48a93cfa319907263a0c9061
fruitgushers <jeremyadams166@gmail.com>
2024-07-28 10:42:19 -0700

    Fixed all

--- app/frontend/src/JS/common/divisions.js
+++ app/frontend/src/JS/common/divisions.js
@@ -3,7 +3,7 @@ const divisions = [
     {"code":"MATH", "label":"Mathmatics"},
     {"code":"PHYS", "label":"Physics"},
     {"code":"STAT", "label":"Statistics"},
-    {"code":"ALL", "label":"ALL"}
+    {"code":"ALL", "label":"All"}
 ];
 
 export default divisions;
