9c3bad22dc8333495b3ce04f1400e38b887f985a
fruitgushers <jeremyadams166@gmail.com>
2024-07-28 14:38:48 -0700

    Refactored login page

--- test/Frontend/All/Login.test.js
+++ test/Frontend/All/Login.test.js
@@ -1,4 +1,4 @@
-import {render, screen } from '@testing-library/react';
+import {render, screen, act, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 import Login from '../../../app/frontend/src/JS/All/Login';
@@ -9,9 +9,25 @@ import axios from 'axios';
 jest.mock('../../../app/frontend/src/JS/common/AuthContext');
 jest.mock('axios');
 
-useAuth.mockResolvedValue('MOCK');
-axios.get.mockResolvedValue(':)');
-axios.post.mockResolvedValue(":(");
+  
+useAuth.mockReturnValue({
+    login:()=>{return 0}
+});
+  
+
+function mockData() {
+    axios.post.mockResolvedValue({
+        data:{success:true, token:"token", expiresIn:"idk", email:"john.doe@ubc.ca", accountId:1, profileId:1}
+    });
+    axios.get.mockResolvedValue({
+        data:{success:true, accountTypes:[1]}
+    });
+}
+
+beforeEach(async()=>{
+    global.alert=jest.fn();
+    mockData();
+});
 
 test('Check user input', async () => {
     const user = userEvent.setup();
@@ -26,4 +42,24 @@ test('Check login button exists', async () => {
     render(<MemoryRouter><Login/></MemoryRouter>);
     const button = screen.getByRole("button");
     expect(button).toBeInTheDocument()
+});
+
+test('Check login success', async () => {
+    console.log = jest.fn();
+ 
+  const logspy = jest.spyOn(global.console, 'log');
+
+  console.log(logspy);
+    const user = userEvent.setup();
+    await act(async () => {
+       render(<MemoryRouter><Login/></MemoryRouter>); 
+    });
+    const emailBox = screen.getByPlaceholderText('Email Address');
+    const passwordBox = screen.getByPlaceholderText("Password");
+    const loginButton = screen.getByRole("button");
+    await user.type(emailBox, "john.doe@ubc.ca");
+    await user.type(passwordBox, "123");
+    await user.click(loginButton);
+    expect(console.log).toBeCalledWith(`Log in as account type: 1`)
+
 });
