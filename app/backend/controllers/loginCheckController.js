8e21835a2f41a1325b4e07c126d51834bc81cbcc
subarusakashita <77289992+subarusakashita@users.noreply.github.com>
2024-07-30 07:38:33 +0900

    Update loginCheckController.js

--- app/backend/controllers/loginCheckController.js
+++ app/backend/controllers/loginCheckController.js
@@ -1,5 +1,5 @@
 const passport = require('passport');
-require('../services/passport-config');  // This imports and configures the passport strategies
+require('../services/passport-config');
 const { generateToken, TOKEN_EXPIRY_SECONDS } = require('../Manager/jwtManager');
 async function loginCheck (req, res, next)  {
     passport.authenticate('local', { session: false }, (err, user, info) => {
