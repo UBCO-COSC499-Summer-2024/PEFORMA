e9b477cdcd46a1e075c5bf73542d599779af22fc
subarusakashita <77289992+subarusakashita@users.noreply.github.com>
2024-07-29 09:55:23 +0900

    Update assignInstructorServiceRoleController.js

--- app/backend/controllers/assignInstructorServiceRoleController.js
+++ app/backend/controllers/assignInstructorServiceRoleController.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const router = express.Router();
 const assignServiceRoleService = require('../services/assignServiceRole');
 
 async function assignServiceRole (req, res) {
