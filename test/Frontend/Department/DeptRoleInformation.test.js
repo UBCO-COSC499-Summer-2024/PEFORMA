ca5cdf7a9b0b127e273bb554bfd614cd9f41f055
fruitgushers <jeremyadams166@gmail.com>
2024-07-30 13:12:14 -0700

    Got started on test

--- test/Frontend/Department/DeptRoleInformation.test.js
+++ test/Frontend/Department/DeptRoleInformation.test.js
@@ -1,4 +1,4 @@
-import {act, render, screen, waitFor } from '@testing-library/react';
+import {act, getByTestId, render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import DeptRoleInformation from '../../../app/frontend/src/JS/Department/DeptRoleInformation';
 import {MemoryRouter} from "react-router-dom";
@@ -12,13 +12,32 @@ useAuth.mockReturnValue({
   accountType: { accountType: 'mocked-accountType' },
 });
 
-axios.get.mockImplementation((url) => {
-    if (url == 'http://localhost:3000/assignees.json' || url == 'http://localhost:3001/api/roleInfo') {
-      return Promise.resolve({"data":{"assignees":[{}], assigneeCount:0, perPage: 5, currentPage: 1}});
-    } else {
-      return Promise.resolve({"data": {"instructors":[{}], instructorCount:0, perPage: 8, currentPage: 1}});
+function mockData() {
+  axios.get.mockImplementation((url) => {
+    if (url == 'http://localhost:3001/api/roleInfo') {
+      return Promise.resolve({"data":{perPage: 5, currentPage: 1, roleID:1, assigneeCount:7, roleName:"Cheese", roleDescription: "Fish",
+                                      department:"Computer Science", benchmark:0, latestYear:20234,
+                                      assignees:[
+                                        {instructorID: '12341234', name:"Mister Test1", year:2023},
+                                        {instructorID: '12341235', name:"Mister Test2", year:2023},
+                                        {instructorID: '12341236', name:"Mister Test3", year:2023},
+                                        {instructorID: '12341237', name:"Mister Test4", year:2023},
+                                        {instructorID: '12341238', name:"Mister Test5", year:2023},
+                                        {instructorID: '12341239', name:"Mister Test6", year:2023}
+                                      ]}});
+    } else if (url == "http://localhost:3001/api/terms") {
+      return Promise.resolve({"data": {currentTerm:20234}});
     }
   });
+  return;
+}
+
+beforeEach(async() => {
+  mockData();
+  global.confirm = () => true;
+  global.alert = jest.fn();
+});
+
 
 test('Checks if buttons exist', async () => {
     await act(async () => {
@@ -27,9 +46,10 @@ test('Checks if buttons exist', async () => {
     const buttons = await screen.getAllByRole('button');
     expect(buttons.length).toBe(6);
 });
-test('Checks if data exists', async () => {
+
+test('Checks if data is properly rendered', async () => {
     await act(async () => {
-        render(<MemoryRouter><DeptRoleInformation/></MemoryRouter>);
+        ({ getByTestId } = render(<MemoryRouter><DeptRoleInformation/></MemoryRouter>));
     });
     
     const data = await screen.getAllByRole("contentinfo");
