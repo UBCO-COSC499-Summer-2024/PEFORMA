ce41a94d3f3c545f7bccfa330df9b134b52ee8cb
Kevin Kim <rkdgus990503@gmail.com>
2024-07-30 13:46:47 -0700

    commenting and removing unused mock accountLogInType

--- test/Frontend/Instructor/InsLeaderBoardChart.test.js
+++ test/Frontend/Instructor/InsLeaderBoardChart.test.js
@@ -1,4 +1,4 @@
-import { render, element, waitFor } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import InsLeaderBoard from '../../../app/frontend/src/JS/Instructor/InsPerformanceImports/InsLeaderBoard';
 import { MemoryRouter } from 'react-router-dom';
@@ -6,29 +6,29 @@ import axios from 'axios';
 import { useAuth } from '../../../app/frontend/src/JS/common/AuthContext';
 import { __esModule } from '@babel/preset-env';
 
+// mocking axios
 jest.mock('axios');
 jest.mock('../../../app/frontend/src/JS/common/AuthContext');
-jest.mock('react-apexcharts', () => ({
+jest.mock('react-apexcharts', () => ({ // mocking react-apexcharts module 
 	__esModule: true,
 	default: () => <div />
 }));
 
 describe('InsLeaderBoard', () => {
-	beforeEach(() => {
-		useAuth.mockReturnValue({
-			accountType: { accountType: 'mocked-accountType' },
+	beforeEach(() => { 
+		useAuth.mockReturnValue({ // mocking authToken for axios.get
 			authToken: { token: 'mocked-token' },
 		});
 	});
 
 	test('Testing apexchart to be called', async () => {
-
+		// render InsLeaderBoard
 		render(
 			<MemoryRouter>
 				<InsLeaderBoard />
 			</MemoryRouter>
 		);
-
+		// waiting for axios.get to be called once, giving us the info that the chart used axios.get to fetch data
 		await waitFor(() => expect(axios.get).toHaveBeenCalledTimes(1));
 	});
 });
