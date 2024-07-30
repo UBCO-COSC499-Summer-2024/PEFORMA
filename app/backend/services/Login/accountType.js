7b176b2047dd9f63fa87711e558d7c096d75715c
subarusakashita <77289992+subarusakashita@users.noreply.github.com>
2024-07-30 14:33:25 +0900

    Login comments

--- app/backend/services/Login/accountType.js
+++ app/backend/services/Login/accountType.js
@@ -3,6 +3,7 @@ const pool = require('../../db/index.js');
 async function queryAccountType(accountId) {
   try {
     const client = await pool.connect();
+    //Retrieve all account type of the user
     const result = await client.query('SELECT "accountType" FROM public."AccountType" WHERE "accountId" = $1', [accountId]);
     client.release();
     if (result.rows.length > 0) {
--- app/backend/services/Login/queryAccount.js
+++ app/backend/services/Login/queryAccount.js
@@ -1,6 +1,7 @@
 const pool = require('../../db/index');
 async function queryAccount(){
     try {
+      //Get all accounts
         const client = await pool.connect();
         const result = await client.query('SELECT * FROM public."Account"'); 
         client.release();
