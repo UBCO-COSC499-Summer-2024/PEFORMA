83617e13b9632cdcdaaa84c48d9716088ad83efd
subarusakashita <77289992+subarusakashita@users.noreply.github.com>
2024-08-01 12:57:03 +0900

    Fixed data import

--- app/backend/services/CourseEvaluation/courseEvaluation.js
+++ app/backend/services/CourseEvaluation/courseEvaluation.js
@@ -23,26 +23,26 @@ async function getCourseEvaluation(req) {
         console.error('Failed to update course evaluation:', error);
         throw error;
     }
-}
+};
 
 async function checkExistingEvaluation(courseId, term, profileId) {
     const query = `SELECT * FROM "CourseEvaluation" WHERE "courseId" = $1 AND "term" = $2 AND "profileId" = $3`;
     const result = await pool.query(query, [courseId, term, profileId]);
     return result.rows.length > 0;
-}
+};
 
 async function insertCourseEvaluation(courseId, term, profileId, Q1, Q2, Q3, Q4, Q5, Q6, retentionRate, failRate, enrolRate, averageGrade) {
     const query = `INSERT INTO "CourseEvaluation" ("courseId", "term", "profileId", "SEIQ1", "SEIQ2", "SEIQ3", "SEIQ4", "SEIQ5", "SEIQ6" , "retentionRate", "failRate", "enrolRate", "averageGrade") 
                 VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) RETURNING *`;
     await pool.query(query, [courseId, term, profileId, Q1, Q2, Q3, Q4, Q5, Q6, retentionRate, failRate, enrolRate, averageGrade]);
-}
+};
 
 async function updateCourseEvaluation(courseId, term, profileId, Q1, Q2, Q3, Q4, Q5, Q6, retentionRate, failRate, enrolRate, averageGrade) {
     const query = `UPDATE "CourseEvaluation" SET "SEIQ1" = $1, "SEIQ2" = $2, "SEIQ3" = $3, "SEIQ4" = $4, "SEIQ5" = $5, "SEIQ6" = $6
                 "retentionRate" = $7, "failRate" = $8, "enrolRate" = $9, "averageGrade" = $10
                 WHERE "courseId" = $11 AND "term" = $12 AND "profileId" = $13 RETURNING *`;
     await pool.query(query, [Q1, Q2, Q3, Q4, Q5, Q6, retentionRate, failRate, enrolRate, averageGrade, courseId, term, profileId]);
-}
+};
 
 async function updateTeachingPerformance(courseId, term, profileId, Q1, Q2, Q3, Q4, Q5, Q6 , retentionRate, averageGrade, enrolRate, failRate) {
     const score = computeScore(Q1, Q2, Q3, Q4, Q5, Q6, retentionRate, averageGrade, enrolRate, failRate);
@@ -57,7 +57,7 @@ async function updateTeachingPerformance(courseId, term, profileId, Q1, Q2, Q3, 
         const queryUpdate = `UPDATE "SingleTeachingPerformance" SET "score" = $1 WHERE "courseId" = $2 AND "term" = $3 AND "profileId" = $4`;
         await pool.query(queryUpdate, [score, courseId, term, profileId]);
     }
-}
+};
 
 function computeScore(Q1, Q2, Q3, Q4, Q5, retentionRate, averageGrade, enrolRate, failRate) {
     const averageQuestionScore = (parseFloat(Q1)+parseFloat(Q2)+parseFloat(Q3)+parseFloat(Q4)+parseFloat(Q5))/5;
@@ -69,10 +69,10 @@ function computeScore(Q1, Q2, Q3, Q4, Q5, retentionRate, averageGrade, enrolRate
 
     finalScore = Math.max(0, Math.min(100, finalScore));
     return finalScore;
-}
+};
 
 
 
 module.exports = {
-    getCourseEvaluation
+    getCourseEvaluation, updateTeachingPerformance
 };
