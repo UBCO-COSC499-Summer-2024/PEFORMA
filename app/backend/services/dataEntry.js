16f3f66f5cfe60e3ac9a453edc75a7dd98f824bb
subarusakashita <77289992+subarusakashita@users.noreply.github.com>
2024-08-03 14:32:29 +0900

    Update dataEntry.js

--- app/backend/services/dataEntry.js
+++ app/backend/services/dataEntry.js
@@ -4,9 +4,10 @@ async function saveDataToDatabase(data) {
     let division;
     let query;
     let result;
-    console.log(data);
+    //If the user clicks service role
     if (data.selection === 'Service Role'){
         const {serviceRoleTitle,serviceRoleDepartment,serviceRoleDescription} = data;
+        //convert the user input to corresponding divisionId
         switch(serviceRoleDepartment){
             case 'COSC': division=[1];break;
             case 'MATH': division=[2];break;
@@ -15,6 +16,7 @@ async function saveDataToDatabase(data) {
             case 'All' : division = [1,2,3,4];break;
             default: division = [];
         }
+        //Check if service role already exists
         query = `SELECT * FROM "ServiceRole" WHERE "divisionId" = ANY($1) AND "stitle" = $2;`;
         result = await pool.query(query,[division,serviceRoleTitle]);
         if(result.rows.length > 0){
@@ -22,9 +24,12 @@ async function saveDataToDatabase(data) {
         }
         await pool.query('BEGIN');
         for(let div of division){
+            //Data for the soon to be inserted data
             const values = [serviceRoleTitle,serviceRoleDescription,true,div];
+            //Manual increment 
             query = `SELECT setval(pg_get_serial_sequence('"ServiceRole"', 'serviceRoleId'), COALESCE((SELECT MAX("serviceRoleId") FROM "ServiceRole"), 0) + 1, false);`;
             await pool.query(query);
+            //Insert the data 
             query = `INSERT INTO public."ServiceRole"("stitle", "description", "isActive", "divisionId") VALUES($1, $2, $3, $4) RETURNING "serviceRoleId";`;        
             try {
                 await pool.query(query, values);
@@ -37,24 +42,30 @@ async function saveDataToDatabase(data) {
 
         }
         await pool.query('COMMIT');
+    //For course creation
     }else{
 
         const { courseTitle, courseDepartment, courseCode, courseDescription } = data;
+        //convert the user input to corresponding divisionId
         switch(courseDepartment){
             case 'COSC': division=1; break;
             case 'MATH' : division=2;break;
             case 'PHYS' : division=3;break;
             case 'STAT':division=4;break;
         }
+        //Check if course alaready exists
         query = `SELECT * FROM "Course" WHERE "divisionId" = $1 AND "courseNum" = $2; `;
         result = await pool.query(query, [division, courseCode]);
         if(result.rows.length > 0){
             throw new Error("Course already exist");
         }
+        //Manual increment of the courseid
         query = `SELECT setval(pg_get_serial_sequence('"Course"', 'courseId'), COALESCE((SELECT MAX("courseId") FROM "Course"), 0) + 1, false);`;
         await pool.query(query);
         await pool.query('BEGIN');
+        //Prep the soon to be inserted data
         const values = [courseTitle, courseDescription, division, courseCode];
+        //Insert data
         query = `INSERT INTO public."Course"(ctitle , description , "divisionId" , "courseNum", "isActive") VALUES($1, $2, $3, $4, true) RETURNING "courseId";`;
         try{
             const data = await pool.query(query,values);
@@ -62,7 +73,6 @@ async function saveDataToDatabase(data) {
             return data;
         }
         catch(error){
-            console.log("Error executing insert function");
             await pool.query('ROLLBACK');
             throw error;
         }
