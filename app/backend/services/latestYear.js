3843dcef5aefc2d282c468b37a2ba89516f8b7ca
subarusakashita <77289992+subarusakashita@users.noreply.github.com>
2024-08-03 14:35:21 +0900

    added comments

--- app/backend/services/latestYear.js
+++ app/backend/services/latestYear.js
@@ -1,6 +1,8 @@
 const {getLatestTerm} = require('./latestTerm');
 async function getLatestYear(){
+    //Get the current term
     const latestTerm = await getLatestTerm();
+    //Retrieve the first four letters for the year
     const latestYear = latestTerm.toString().substring(0, 4);
     return latestYear;
 };
